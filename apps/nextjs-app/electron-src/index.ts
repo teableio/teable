d6e49f4ed916fdb3f188afaf7a82b8630bf1d712
thegreattoymaker <thegreattoymaker@teable.io>
2022-11-09 18:54:39 +0800

    feat: add electron-app(dev)

--- apps/nextjs-app/electron-src/index.ts
+++ apps/nextjs-app/electron-src/index.ts
@@ -1,22 +1,31 @@
 // Native
-import { join } from 'path';
 
 // Packages
-import { BrowserWindow, app, ipcMain, IpcMainEvent } from 'electron';
-import isDev from 'electron-is-dev';
-
 import { createServer } from 'http';
+import { join } from 'path';
 import { parse } from 'url';
+import type { IpcMainEvent } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
+import isDev from 'electron-is-dev';
+import log from 'electron-log';
 import next from 'next';
 
-const nextAppDir = join(app.getAppPath(), '../nextjs-app');
+const nextAppDir = app.getAppPath();
 const nextApp = next({ dev: isDev, dir: nextAppDir });
 const handle = nextApp.getRequestHandler();
+log.info('log ok');
 
 // Prepare the renderer once the app is ready
 app.on('ready', async () => {
-  await nextApp.prepare();
+  log.info('app ready');
+  try {
+    await nextApp.prepare();
+  } catch (error) {
+    log.error(error);
+  }
 
+  log.info('nextjs start');
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   createServer((req: any, res: any) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
@@ -41,6 +50,7 @@ app.on('ready', async () => {
 app.on('window-all-closed', app.quit);
 
 // listen the channel `message` and resend the received message to the renderer process
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 ipcMain.on('message', (event: IpcMainEvent, message: any) => {
   console.log(message);
   setTimeout(() => event.sender.send('message', 'hi from electron'), 500);
--- apps/nextjs-app/electron-src/preload.ts
+++ apps/nextjs-app/electron-src/preload.ts
@@ -1,8 +1,10 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { ipcRenderer, IpcRenderer } from 'electron';
+import type { IpcRenderer } from 'electron';
+import { ipcRenderer } from 'electron';
 
 declare global {
+  // eslint-disable-next-line no-var
   var ipcRenderer: IpcRenderer;
 }
 
