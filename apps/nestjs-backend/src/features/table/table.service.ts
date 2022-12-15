364a3d2e01ae5d0100f110a8cf49b71210ce2397
tea artist <artist@teable.io>
2022-12-15 16:01:25 +0800

    refactor: clearly refactor table creation flow

--- apps/nestjs-backend/src/features/table/table.service.ts
+++ apps/nestjs-backend/src/features/table/table.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import type { TableMeta, Prisma } from '@prisma/client';
 import { PrismaService } from '../../prisma.service';
-import { generateTableId, generateViewId } from '../../utils/id-generator';
+import { generateTableId } from '../../utils/id-generator';
 import { convertNameToValidCharacter } from '../../utils/name-conversion';
 import { FieldService } from '../field/field.service';
 import { ViewService } from '../view/view.service';
@@ -23,11 +23,38 @@ export class TableService {
     return `${tableNamePrefix}_${validInputName}`;
   }
 
-  async createTable(createTableDto: CreateTableDto): Promise<TableMeta> {
+  private async createDefaultField(prisma: Prisma.TransactionClient, tableId: string) {
+    const fieldPromise = await this.fieldService.generateMultipleCreateFieldPromise(
+      prisma,
+      tableId,
+      DEFAULT_FIELDS
+    );
+
+    for (const index in fieldPromise) {
+      const result = await fieldPromise[index];
+      console.log(`field task ${+index + 1} / ${fieldPromise.length} succeed: `, result);
+    }
+  }
+
+  private async createDefaultView(prisma: Prisma.TransactionClient, tableId: string) {
+    const viewPromise = await this.viewService.generateCreateViewPromise(
+      prisma,
+      tableId,
+      DEFAULT_VIEW
+    );
+
+    for (const index in viewPromise) {
+      const result = await viewPromise[index];
+      console.log(`view task ${+index + 1} / ${viewPromise.length} succeed: `, result);
+    }
+  }
+
+  private async createDBTable(
+    prisma: Prisma.TransactionClient,
+    tableId: string,
+    createTableDto: CreateTableDto
+  ) {
     const validDbTableName = this.generateValidDbTableName(createTableDto.name);
-    const tableId = generateTableId();
-    const viewId = generateViewId();
-    const rowIndexFieldName = this.viewService.getRowIndexFieldName(viewId);
     const dbTableName = `${validDbTableName}_${tableId}`;
     const data: Prisma.TableMetaCreateInput = {
       id: tableId,
@@ -36,55 +63,39 @@ export class TableService {
       createdBy: 'admin',
       lastModifiedBy: 'admin',
     };
+    const tableMeta = await prisma.tableMeta.create({
+      data,
+    });
+    console.log('table meta create succeed: ', tableMeta);
+
+    // 2. create a real db table
+    const dbTable = await prisma.$executeRawUnsafe(`
+      CREATE TABLE ${dbTableName} (
+        __id TEXT NOT NULL UNIQUE,
+        __autoNumber INTEGER PRIMARY KEY AUTOINCREMENT,
+        __createdTime DATETIME,
+        __lastModifiedTime DATETIME,
+        __createdBy TEXT,
+        __lastModifiedBy TEXT
+      );
+    `);
+    console.log('dbTable create succeed: ', dbTable);
+    return tableMeta;
+  }
+
+  async createTable(createTableDto: CreateTableDto): Promise<TableMeta> {
+    const tableId = generateTableId();
 
     return await this.prisma.$transaction(async (prisma) => {
-      // 1. create table meta
-      const tableMeta = await prisma.tableMeta.create({
-        data,
-      });
+      // 1. create db table
 
-      console.log('table meta create succeed: ', tableMeta);
+      const tableMeta = await this.createDBTable(prisma, tableId, createTableDto);
 
-      // 2. create a real db table
-      const dbTable = await prisma.$executeRawUnsafe(`
-        CREATE TABLE ${dbTableName} (
-          __id TEXT NOT NULL UNIQUE,
-          __autoNumber INTEGER PRIMARY KEY AUTOINCREMENT,
-          ${rowIndexFieldName} NOT NULL UNIQUE,
-          __createdTime DATETIME,
-          __lastModifiedTime DATETIME,
-          __createdBy TEXT,
-          __lastModifiedBy TEXT
-        );
-      `);
+      // 2. create field for table
+      await this.createDefaultField(prisma, tableId);
 
-      console.log('dbTable create succeed: ', dbTable);
-
-      // 3. create field for table
-      const fieldPromise = await this.fieldService.generateMultipleCreateFieldPromise(
-        prisma,
-        tableId,
-        DEFAULT_FIELDS
-      );
-
-      for (const index in fieldPromise) {
-        const result = await fieldPromise[index];
-        console.log(`field task ${+index + 1} / ${fieldPromise.length} succeed: `, result);
-      }
-
-      // console.log('fields create succeed: ', fieldData);
-
-      // 4. create view for table
-      const viewPromise = await this.viewService.generateCreateViewPromise(
-        prisma,
-        tableId,
-        DEFAULT_VIEW
-      );
-
-      for (const index in viewPromise) {
-        const result = await viewPromise[index];
-        console.log(`view task ${+index + 1} / ${viewPromise.length} succeed: `, result);
-      }
+      // 3. create view for table
+      await this.createDefaultView(prisma, tableId);
 
       return tableMeta;
     });
