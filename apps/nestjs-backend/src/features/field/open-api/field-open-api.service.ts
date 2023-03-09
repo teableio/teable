e957f8c3dc874ef9e7bb31f2946b469ad7596322
tea artist <artist@teable.io>
2023-03-09 20:47:38 +0800

    refactor: remove useless method in create field

--- apps/nestjs-backend/src/features/field/open-api/field-open-api.service.ts
+++ apps/nestjs-backend/src/features/field/open-api/field-open-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import type { IColumnMeta, IFieldSnapshot, IOtOperation } from '@teable-group/core';
+import type { IFieldSnapshot } from '@teable-group/core';
 import { OpBuilder } from '@teable-group/core';
 import { PrismaService } from '../../../prisma.service';
 import { ShareDbService } from '../../../share-db/share-db.service';
@@ -20,17 +20,7 @@ export class FieldOpenApiService {
     await this.prismaService.$transaction(async (prisma) => {
       this.transactionService.set(tableId, prisma);
       try {
-        const doc = await this.shareDbService.createDocument(
-          tableId,
-          fieldId,
-          result.createSnapshot
-        );
-        await new Promise((resolve, reject) => {
-          doc.submitOp([result.fieldOperation], undefined, (error) => {
-            if (error) return reject(error);
-            resolve(undefined);
-          });
-        });
+        await this.shareDbService.createDocument(tableId, fieldId, result.createSnapshot);
       } finally {
         this.transactionService.remove(tableId);
       }
@@ -42,39 +32,13 @@ export class FieldOpenApiService {
     fieldInstance: IFieldInstance
   ): Promise<{
     createSnapshot: IFieldSnapshot;
-    fieldOperation: IOtOperation;
   }> {
-    const fieldsData = await this.prismaService.field.findMany({
-      where: { tableId: tableId },
-      select: { id: true, columnMeta: true },
-    });
-
-    const defaultView = await this.prismaService.view.findFirstOrThrow({
-      where: { tableId },
-      select: { id: true },
-    });
-    const defaultViewId = defaultView.id;
-
-    const maxFieldOrder = fieldsData.reduce((max, fieldData) => {
-      const columnMeta: IColumnMeta = JSON.parse(fieldData.columnMeta);
-      const order = columnMeta[defaultViewId].order;
-      return Math.max(max, order);
-    }, -1);
-
     const createSnapshot = OpBuilder.creator.addField.build({
       ...fieldInstance,
     });
 
-    // we only need build column in default view here
-    // because we will build all columns after submit
-    const fieldOperation = OpBuilder.editor.addColumnMeta.build({
-      viewId: defaultViewId,
-      newMetaValue: { order: maxFieldOrder + 1 },
-    });
-
     return {
       createSnapshot,
-      fieldOperation,
     };
   }
 }
