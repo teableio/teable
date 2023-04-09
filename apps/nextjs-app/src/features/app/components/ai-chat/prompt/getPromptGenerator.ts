dee28a66ac62e79f397a7183f84d7a89ebaf6b8b
tea artist <artist@teable.io>
2023-04-09 18:37:06 +0800

    feat: optimize prompt for tai-app

---
+++ apps/nextjs-app/src/features/app/components/ai-chat/prompt/getPromptGenerator.ts
@@ -0,0 +1,18 @@
+import type { IUser } from 'store/user';
+
+// getPromptOfAssistant define the special prompt for each assistant.
+export function getPromptGeneratorOfAssistant(
+  assistant: IUser
+): (promptContext?: string) => string {
+  const basicPrompt = `Your name is Tai, and you are an AI assistant for Teable, Please be careful to return only key information, and try not to make it too long.
+Set the programming language to the markdown code block for each code block. For example, \`let a = 1\` is JavaScript.
+Use the same natural language to respond as the one used for asking the question.
+\n`;
+  if (assistant.id === 'tai-app') {
+    return (doc?: string) =>
+      `${basicPrompt}, This is Table API doc: "${doc}". \n And please use JavaScript and fetch method to generate code to answer the following questions. 
+If the question mentions tables, systems, or databases, you can assume that it is referring to creating a table. 
+\n`;
+  }
+  return () => `\n${basicPrompt}`;
+}
