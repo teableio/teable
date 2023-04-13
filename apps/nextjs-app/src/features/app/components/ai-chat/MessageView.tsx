9ae3392f3ecd0d45ce510ee93363698b1d811e1a
tea artist <artist@teable.io>
2023-04-14 00:35:21 +0800

    feat: add ai process animation

--- apps/nextjs-app/src/features/app/components/ai-chat/MessageView.tsx
+++ apps/nextjs-app/src/features/app/components/ai-chat/MessageView.tsx
@@ -10,6 +10,7 @@ import { useUserStore } from 'store/user';
 import { RenderBox } from '../render-box/RenderBox';
 import { CodeBlock } from './CodeBlock';
 import { createAISyntaxParser } from './createAISyntaxParser';
+import { ProcessBar } from './ProcessBar';
 import type { IChat } from './type';
 dayjs.extend(localizedFormat);
 
@@ -23,7 +24,55 @@ export const MessageView: React.FC<Props> = ({ message, chat }) => {
   const isCurrentUser = message.creatorId === userStore.currentUser.id;
   const isOfficial = message.creatorId === 'teable';
   const [parser] = useState(() => createAISyntaxParser());
+  const isAiCode = message.content.includes('```ai');
+  const [debugAI, setDebugAI] = useState(false);
+  const Element = () => {
+    if (isAiCode && !debugAI) {
+      const regex = /```ai\n([\s\S]*?)(?:(```)|$)/;
+      const match = message.content.match(regex);
+      let done = false;
+      if (match) {
+        const content = match[1];
+        console.log(match);
+        done = Boolean(match[2]);
+        parser(content);
+      }
+      return <ProcessBar done={done} onClick={() => setDebugAI(true)} />;
+    }
 
+    if (isOfficial && message.code) {
+      return <RenderBox code={message.code} />;
+    }
+
+    return (
+      <ReactMarkdown
+        className="w-auto max-w-full bg-base-300 px-2 py-1 rounded-lg prose prose-slate text-sm"
+        remarkPlugins={[remarkGfm]}
+        components={{
+          pre({ node, className, children, ...props }) {
+            const child = children[0] as ReactElement;
+            const match = /language-(\w+)/.exec(child.props.className || '');
+            const language = match ? match[1] : 'text';
+            const strValue = String(child.props.children);
+            return (
+              <pre className={`${className || ''} w-full p-0 my-1`} {...props}>
+                <CodeBlock chat={chat} language={language || 'text'} value={strValue} {...props} />
+              </pre>
+            );
+          },
+          code({ children, key }) {
+            return (
+              <code key={key} className="px-0">
+                `{children}`
+              </code>
+            );
+          },
+        }}
+      >
+        {message.content}
+      </ReactMarkdown>
+    );
+  };
   return (
     <div
       className={`group w-full max-w-full flex flex-row justify-start items-start my-4 ${
@@ -42,42 +91,7 @@ export const MessageView: React.FC<Props> = ({ message, chat }) => {
       ) : (
         <>
           <div className="w-full flex flex-col justify-start items-start">
-            {isOfficial && message.code ? (
-              <RenderBox code={message.code} />
-            ) : (
-              <ReactMarkdown
-                className="w-auto max-w-full bg-base-300 px-2 py-1 rounded-lg prose prose-slate text-sm"
-                remarkPlugins={[remarkGfm]}
-                components={{
-                  pre({ node, className, children, ...props }) {
-                    const child = children[0] as ReactElement;
-                    const match = /language-(\w+)/.exec(child.props.className || '');
-                    const language = match ? match[1] : 'text';
-                    const strValue = String(child.props.children);
-                    parser(strValue);
-                    return (
-                      <pre className={`${className || ''} w-full p-0 my-1`} {...props}>
-                        <CodeBlock
-                          chat={chat}
-                          language={language || 'text'}
-                          value={strValue}
-                          {...props}
-                        />
-                      </pre>
-                    );
-                  },
-                  code({ children, key }) {
-                    return (
-                      <code key={key} className="px-0">
-                        `{children}`
-                      </code>
-                    );
-                  },
-                }}
-              >
-                {message.content}
-              </ReactMarkdown>
-            )}
+            {Element()}
             <span className="self-end text-xs pt-1 pr-1">
               {dayjs(message.createdAt).format('lll')}
             </span>
