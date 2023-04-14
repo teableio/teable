b6d0227d31bc96b9718865609ab25b7d6139fa30
boris <boris2code@outlook.com>
2023-04-14 15:03:50 +0800

    feat: add generate-chart operation

---
+++ apps/nextjs-app/src/features/app/components/Chart/Chart.tsx
@@ -0,0 +1,48 @@
+import * as echarts from 'echarts';
+import { useCallback, useEffect, useRef } from 'react';
+import type { Bar } from './bar';
+import type { Line } from './line';
+import type { Pie } from './pie';
+
+export const Chart = (props: { chartInstance: Pie | Bar | Line }) => {
+  const { chartInstance } = props;
+  const chartContainerRef = useRef<HTMLDivElement>(null);
+
+  const renderEcharts = useCallback(
+    ({ width, height }: { width: number; height: number }) => {
+      if (!chartContainerRef.current) {
+        return;
+      }
+      // eslint-disable-next-line import/namespace
+      const myChart = echarts.init(chartContainerRef.current);
+      console.log(chartInstance.getOptions());
+      myChart.setOption(chartInstance.getOptions());
+      myChart.resize({ width, height });
+    },
+    [chartInstance]
+  );
+
+  useEffect(() => {
+    const resizeObserver = new ResizeObserver((entries) => {
+      entries.forEach((entry) => {
+        renderEcharts({ width: entry.contentRect.width, height: entry.contentRect.height });
+      });
+    });
+
+    if (chartContainerRef.current) {
+      resizeObserver.observe(chartContainerRef.current);
+    }
+
+    return () => {
+      resizeObserver.disconnect();
+    };
+  }, [chartInstance, renderEcharts]);
+
+  return (
+    <div
+      ref={chartContainerRef}
+      className={'w-full h-full p-2	overflow-hidden	'}
+      style={{ minHeight: '300px', minWidth: '200px' }}
+    />
+  );
+};
