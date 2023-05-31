db731afa9732e540624b1fde034fd2aa51711618
boris <boris2code@outlook.com>
2023-05-31 11:21:52 +0800

    fix: initializing the theme does not work

--- apps/nextjs-app/src/themes/initial.ts
+++ apps/nextjs-app/src/themes/initial.ts
@@ -1,11 +1,11 @@
 export const INITIAL_THEME = `
   function getUserPreference() {
-    if(window.localStorage.getItem('theme')) {
-      return window.localStorage.getItem('theme')
+    if(window.localStorage.getItem('teable_theme')) {
+      return JSON.parse(window.localStorage.getItem('teable_theme'))
     }
     return window.matchMedia('(prefers-color-scheme: dark)').matches
               ? 'dark'
               : 'light'
   }
-  document.body.dataset.theme = getUserPreference();
+  document.documentElement.setAttribute('data-theme', getUserPreference());
 `;
