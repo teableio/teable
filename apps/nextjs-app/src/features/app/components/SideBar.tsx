2a0270f35749adb349d4af422839c7082a124394
boris <boris2code@outlook.com>
2023-06-14 15:15:35 +0800

    fix: connection status loading

--- apps/nextjs-app/src/features/app/components/SideBar.tsx
+++ apps/nextjs-app/src/features/app/components/SideBar.tsx
@@ -27,7 +27,17 @@ export const SideBar: React.FC = () => {
         />
         <span>teable</span>
         {isHydrated && <ThemePicker />}
-        {!connected && <button className="btn btn-xs btn-ghost loading"></button>}
+        {!connected && (
+          <button className="ml-2 align-middle">
+            <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          </button>
+        )}
       </div>
 
       <div className="divide-base-300 divide-y divide-solid flex flex-col overflow-hidden">
