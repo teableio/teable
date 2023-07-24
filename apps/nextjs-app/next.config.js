b1dfc192bd7ca29f1207d7e32e92aa189d86ddda
boris <boris2code@outlook.com>
2023-07-24 17:29:09 +0800

    chore: changed socket port to 3000 and resolved dependencies for successful build and launch

--- apps/nextjs-app/next.config.js
+++ apps/nextjs-app/next.config.js
@@ -206,7 +206,7 @@ const nextConfig = {
       destination: `http://${NEXTJS_SERVER_HOST}:${NEXTJS_SOCKET_PORT}/socket/:path*`,
     };
 
-    return isProd ? [] : [socketProxy];
+    return isProd ? [] : [];
   },
 
   // @link https://nextjs.org/docs/api-reference/next.config.js/rewrites
