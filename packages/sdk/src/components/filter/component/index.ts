6b132775c5ab8714f8af1c51cbc7c44a2d14a763
xing_cx <caoxing@ones.cn>
2023-07-25 19:08:44 +0800

    chore: update `filterDatePicker` component folder name

--- packages/sdk/src/components/filter/component/index.ts
+++ packages/sdk/src/components/filter/component/index.ts
@@ -2,7 +2,7 @@ export * from './SingleSelect';
 export * from './MultipleSelect';
 export * from './FilterInput';
 export * from './FilterInputNumber';
-export * from './FilterDatePicker';
+export * from './filterDatePicker';
 export * from './FilterUserSelect';
 export * from './FilterCheckBox';
 export * from './FilterLinkSelect';
