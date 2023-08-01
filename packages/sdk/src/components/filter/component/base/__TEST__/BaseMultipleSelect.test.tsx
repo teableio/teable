a2d05612c17a30be5a641fa879662e5be9539507
xing_cx <caoxing9@gmail.com>
2023-08-01 15:43:26 +0800

    test: add filter base component tests

---
+++ packages/sdk/src/components/filter/component/base/__TEST__/BaseMultipleSelect.test.tsx
@@ -0,0 +1,59 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { BaseMultipleSelect } from '../BaseMultipleSelect';
+
+window.ResizeObserver =
+  window.ResizeObserver ||
+  jest.fn().mockImplementation(() => ({
+    disconnect: jest.fn(),
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+  }));
+
+describe('BaseMultipleSelect', () => {
+  const options = [
+    {
+      label: 'label-1',
+      value: 'value-1',
+    },
+    {
+      label: 'label-2',
+      value: 'value-2',
+    },
+    {
+      label: 'label-3',
+      value: 'value-3',
+    },
+    {
+      label: 'label-4',
+      value: 'value-4',
+    },
+  ];
+  const toggleOpen = async () => {
+    fireEvent.click(screen.getByRole('combobox'));
+  };
+  const initvalue = ['value-1'];
+  const onSelect = () => {};
+
+  it('should render init selected value label', async () => {
+    render(<BaseMultipleSelect options={options} onSelect={onSelect} value={initvalue} />);
+    const triggerButton = screen.getByRole('combobox');
+    expect(triggerButton).toHaveTextContent('label-1');
+  });
+
+  it('should render dialog even click option', async () => {
+    render(<BaseMultipleSelect options={options} onSelect={onSelect} value={initvalue} />);
+    toggleOpen();
+    const dialog = screen.getByRole('dialog');
+    fireEvent.click(screen.getAllByRole('option')[0]);
+    expect(dialog).toBeDefined();
+  });
+
+  it('should call onSelect twice', async () => {
+    const selectHandler = jest.fn();
+    render(<BaseMultipleSelect options={options} onSelect={selectHandler} value={null} />);
+    toggleOpen();
+    fireEvent.click(screen.getAllByRole('option')[0]);
+    fireEvent.click(screen.getAllByRole('option')[1]);
+    expect(selectHandler).toHaveBeenCalledTimes(2);
+  });
+});
