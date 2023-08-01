df1f1d51a1917f158156fa913fbf364c517d9ee5
boris <boris2code@outlook.com>
2023-08-01 14:43:27 +0800

    fix: lint error

--- apps/nestjs-backend/src/features/selection/selection.controller.spec.ts
+++ apps/nestjs-backend/src/features/selection/selection.controller.spec.ts
@@ -1,4 +1,5 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import type { TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { SelectionController } from './selection.controller';
 
 describe('SelectionController', () => {
