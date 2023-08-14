a4ea98330e59bca967c22ad59b220facee5318de
boris <boris2code@outlook.com>
2023-08-14 21:27:13 +0800

    feat: the command component supports keyboard up and down selection loops

--- apps/nextjs-app/src/features/app/blocks/grid/components/editor/SelectEditor.tsx
+++ apps/nextjs-app/src/features/app/blocks/grid/components/editor/SelectEditor.tsx
@@ -11,7 +11,6 @@ import classNames from 'classnames';
 import { isString, noop } from 'lodash';
 import { forwardRef, useImperativeHandle, useRef, useState } from 'react';
 import type { ForwardRefRenderFunction } from 'react';
-import { useKeyboardNavigation } from '../../hooks';
 import type { ISelectCell } from '../../renderers';
 import type { IEditorProps, IEditorRef } from './EditorContainer';
 
@@ -23,22 +22,18 @@ const SelectEditorBase: ForwardRefRenderFunction<
   IEditorRef<ISelectCell>,
   IEditorProps<ISelectCell>
 > = (props, ref) => {
-  const { isEditing, cell, style, onChange } = props;
+  const { cell, isEditing, style, onChange } = props;
   const { data, isMultiple, choices = [] } = cell;
   const [values, setValues] = useState(getFormatSelectValue(data));
   const inputRef = useRef<HTMLInputElement | null>(null);
 
-  const activeIndex = useKeyboardNavigation(choices.length, isEditing);
-
   useImperativeHandle(ref, () => ({
-    focus: noop,
+    focus: () => inputRef.current?.focus(),
     setValue: (data: string[]) => {
       const value = getFormatSelectValue(data);
       setValues(value);
     },
-    saveValue: () => {
-      activeIndex > -1 && onSelect(choices[activeIndex].name);
-    },
+    saveValue: noop,
   }));
 
   const onSelect = (v: string) => {
@@ -56,34 +51,30 @@ const SelectEditorBase: ForwardRefRenderFunction<
 
   return (
     <Command className="rounded-sm shadow-sm p-2 border" style={style}>
+      <CommandInput ref={inputRef} placeholder="Search" />
       <CommandList>
-        <CommandInput ref={inputRef} placeholder="Search" />
         <CommandEmpty>No found.</CommandEmpty>
         <CommandGroup aria-valuetext="name">
-          {choices.map(({ bgColor, textColor, name }, index) => (
-            <CommandItem
-              key={name}
-              value={name}
-              className={classNames(activeIndex === index ? 'bg-accent' : 'bg-transparent')}
-              onSelect={() => onSelect(name)}
-            >
-              <SelectIcon
-                className={classNames(
-                  'mr-2 h-4 w-4',
-                  values?.includes(name) ? 'opacity-100' : 'opacity-0'
-                )}
-              />
-              <div
-                className={classNames('px-2 rounded-lg text-ellipsis whitespace-nowrap')}
-                style={{
-                  backgroundColor: bgColor,
-                  color: textColor,
-                }}
-              >
-                {name}
-              </div>
-            </CommandItem>
-          ))}
+          {isEditing &&
+            choices.map(({ bgColor, textColor, name }) => (
+              <CommandItem key={name} value={name} onSelect={() => onSelect(name)}>
+                <SelectIcon
+                  className={classNames(
+                    'mr-2 h-4 w-4',
+                    values?.includes(name) ? 'opacity-100' : 'opacity-0'
+                  )}
+                />
+                <div
+                  className={classNames('px-2 rounded-lg text-ellipsis whitespace-nowrap')}
+                  style={{
+                    backgroundColor: bgColor,
+                    color: textColor,
+                  }}
+                >
+                  {name}
+                </div>
+              </CommandItem>
+            ))}
         </CommandGroup>
       </CommandList>
     </Command>
