f9c3f1a10e5dbe1059870b454708908597abc684
Pengap <penganping_ping@163.com>
2023-09-01 10:55:06 +0800

    chore: upgrade depend (#153)
    Co-authored-by: pengap <penganpingprivte@gmail.com>

--- packages/sdk/src/context/anchor/AnchorProvider.tsx
+++ packages/sdk/src/context/anchor/AnchorProvider.tsx
@@ -1,4 +1,6 @@
 import React, { useMemo } from 'react';
+import { RowCountContext } from '../aggregation/RowCountContext';
+import { RowCountProvider } from '../aggregation/RowCountProvider';
 import { FieldContext, FieldProvider } from '../field';
 import { RecordContext, RecordProvider } from '../record';
 import { ViewContext, ViewProvider } from '../view';
@@ -26,13 +28,17 @@ export const AnchorProvider: React.FC<IAnchorProvider> = ({
       {tableId ? (
         <FieldProvider fallback={fallback}>
           <ViewProvider>
-            <RecordProvider>{children}</RecordProvider>
+            <RecordProvider>
+              <RowCountProvider>{children}</RowCountProvider>
+            </RecordProvider>
           </ViewProvider>
         </FieldProvider>
       ) : (
         <FieldContext.Provider value={{ fields: [] }}>
           <ViewContext.Provider value={{ views: [] }}>
-            <RecordContext.Provider value={{}}>{children}</RecordContext.Provider>
+            <RecordContext.Provider value={{}}>
+              <RowCountContext.Provider value={null}>{children}</RowCountContext.Provider>
+            </RecordContext.Provider>
           </ViewContext.Provider>
         </FieldContext.Provider>
       )}
