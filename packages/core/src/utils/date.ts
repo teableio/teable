72044919b17d18a5f476f7f8fe5cb45640b76930
Sky <906268297@qq.com>
2023-09-14 20:47:29 +0800

    fix: ci crash

--- packages/core/src/utils/date.ts
+++ packages/core/src/utils/date.ts
@@ -1,5 +1,10 @@
 import type { ManipulateType } from 'dayjs';
 import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export class DateUtil {
   public static readonly NORM_YEAR_PATTERN = 'YYYY';
