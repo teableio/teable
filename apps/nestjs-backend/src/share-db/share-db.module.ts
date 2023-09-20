4bc290b191874c2220293a61db17fe45adb0a3c0
boris <boris2code@outlook.com>
2023-09-20 14:52:57 +0800

    feat: add user exist validate in ws-auth

--- apps/nestjs-backend/src/share-db/share-db.module.ts
+++ apps/nestjs-backend/src/share-db/share-db.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../features/auth/auth.module';
 import { CalculationModule } from '../features/calculation/calculation.module';
 import { TableModule } from '../features/table/table.module';
+import { UserModule } from '../features/user/user.module';
 import { DerivateChangeService } from './derivate-change.service';
 import { ShareDbService } from './share-db.service';
 import { SqliteDbAdapter } from './sqlite.adapter';
 import { WsAuthService } from './ws-auth.service';
 
 @Module({
-  imports: [TableModule, CalculationModule, AuthModule],
+  imports: [TableModule, CalculationModule, AuthModule, UserModule],
   providers: [ShareDbService, SqliteDbAdapter, DerivateChangeService, WsAuthService],
   exports: [ShareDbService, WsAuthService],
 })
--- apps/nestjs-backend/src/share-db/ws-auth.service.ts
+++ apps/nestjs-backend/src/share-db/ws-auth.service.ts
@@ -2,10 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import cookie from 'cookie';
 import { AUTH_COOKIE } from '../const';
 import { AuthService } from '../features/auth/auth.service';
+import { UserService } from '../features/user/user.service';
 
 @Injectable()
 export class WsAuthService {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService
+  ) {}
 
   async auth(cookie: string) {
     const token = WsAuthService.extractTokenFromHeader(cookie);
@@ -13,7 +17,12 @@ export class WsAuthService {
       throw new UnauthorizedException();
     }
     try {
-      return await this.authService.validateJwtToken(token);
+      const payload = await this.authService.validateJwtToken(token);
+      const user = await this.userService.getUserById(payload.id);
+      if (!user) {
+        throw new UnauthorizedException();
+      }
+      return { id: user.id };
     } catch (error) {
       throw new UnauthorizedException();
     }
