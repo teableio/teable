f920bfb6b895953d905fe1e3aad7747c6cb79084
boris <boris2code@outlook.com>
2023-10-17 17:18:28 +0800

    feat: redefined copy and paste operations for content from both internal and external sources

--- apps/nextjs-app/src/features/app/blocks/view/grid/hooks/useSelectionOperation.ts
+++ apps/nextjs-app/src/features/app/blocks/view/grid/hooks/useSelectionOperation.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { useMutation } from '@tanstack/react-query';
+import type { ClearRo, ICopyRo, PasteRo } from '@teable-group/openapi';
 import { clear, copy, paste, RangeType } from '@teable-group/openapi';
 import { useTableId, useViewId } from '@teable-group/sdk';
 import { useToast } from '@teable-group/ui-lib';
@@ -12,9 +15,24 @@ const rangeTypes = {
   [SelectionRegionType.None]: undefined,
 };
 
+const copyMark = 'TeableCopyContent:';
+
 export const useSelectionOperation = () => {
   const tableId = useTableId();
   const viewId = useViewId();
+
+  const { mutateAsync: copyReq } = useMutation({
+    mutationFn: (copyRo: ICopyRo) => copy(tableId!, viewId!, copyRo),
+  });
+
+  const { mutateAsync: pasteReq } = useMutation({
+    mutationFn: (pasteRo: PasteRo) => paste(tableId!, viewId!, pasteRo),
+  });
+
+  const { mutateAsync: clearReq } = useMutation({
+    mutationFn: (clearRo: ClearRo) => clear(tableId!, viewId!, clearRo),
+  });
+
   const { toast } = useToast();
 
   const copyHeaderKey = 'teable_copy_header';
@@ -24,6 +42,7 @@ export const useSelectionOperation = () => {
       if (!viewId || !tableId) {
         return;
       }
+
       const toaster = toast({
         title: 'Copying...',
       });
@@ -31,16 +50,17 @@ export const useSelectionOperation = () => {
 
       const type = rangeTypes[selection.type];
 
-      const { data } = await copy(tableId, viewId, {
+      const { data } = await copyReq({
         ranges,
         ...(type ? { type } : {}),
       });
       const { content, header } = data;
-      await navigator.clipboard.writeText(content);
+
+      await navigator.clipboard.writeText(`${copyMark}${content}`);
       sessionStorage.setItem(copyHeaderKey, JSON.stringify(header));
       toaster.update({ id: toaster.id, title: 'Copied success!' });
     },
-    [tableId, toast, viewId]
+    [tableId, toast, viewId, copyReq]
   );
 
   const doPaste = useCallback(
@@ -51,18 +71,19 @@ export const useSelectionOperation = () => {
       const toaster = toast({
         title: 'Pasting...',
       });
-      const headerStr = sessionStorage.getItem(copyHeaderKey);
-      const header = headerStr ? JSON.parse(headerStr) : undefined;
       const ranges = selection.ranges;
       const content = await navigator.clipboard.readText();
-      await paste(tableId, viewId, {
-        content,
+      const usingHeader = content.startsWith(copyMark);
+      const headerStr = sessionStorage.getItem(copyHeaderKey);
+      const header = headerStr && usingHeader ? JSON.parse(headerStr) : undefined;
+      await pasteReq({
+        content: usingHeader ? content.split(copyMark)[1] : content,
         cell: ranges[0],
         header,
       });
       toaster.update({ id: toaster.id, title: 'Pasted success!' });
     },
-    [tableId, toast, viewId]
+    [tableId, toast, viewId, pasteReq]
   );
 
   const doClear = useCallback(
@@ -76,14 +97,14 @@ export const useSelectionOperation = () => {
 
       const type = rangeTypes[selection.type];
 
-      await clear(tableId, viewId, {
+      await clearReq({
         ranges: selection.ranges,
         ...(type ? { type } : {}),
       });
 
       toaster.update({ id: toaster.id, title: 'Clear success!' });
     },
-    [tableId, toast, viewId]
+    [tableId, toast, viewId, clearReq]
   );
 
   return { copy: doCopy, paste: doPaste, clear: doClear };
