3c055fd3e61acd17719cd0939349615186c7b2c6
boris <boris2code@outlook.com>
2023-10-20 18:51:07 +0800

    feat: cellvalue typecast model

---
+++ apps/nestjs-backend/src/features/record/typecast.validate.spec.ts
@@ -0,0 +1,436 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Colors, FieldKeyType, FieldType } from '@teable-group/core';
+import type { PrismaService } from '@teable-group/db-main-prisma';
+import { mockDeep, mockReset } from 'jest-mock-extended';
+import type { FieldConvertingService } from '../field/field-calculate/field-converting.service';
+import type { IFieldInstance } from '../field/model/factory';
+import type { LinkFieldDto } from '../field/model/field-dto/link-field.dto';
+import type { SingleSelectFieldDto } from '../field/model/field-dto/single-select-field.dto';
+import type { RecordService } from './record.service';
+import { TypeCastAndValidate } from './typecast.validate';
+
+jest.mock('zod-validation-error', () => {
+  return {
+    __esModule: true,
+    fromZodError: (message: any) => message,
+  };
+});
+
+describe('TypeCastAndValidate', () => {
+  const prismaService = mockDeep<PrismaService>();
+  const fieldConvertingService = mockDeep<FieldConvertingService>();
+  const recordService = mockDeep<RecordService>();
+
+  const services = {
+    prismaService,
+    fieldConvertingService,
+    recordService,
+  };
+  const tableId = 'tableId';
+
+  afterEach(() => {
+    mockReset(fieldConvertingService);
+    mockReset(prismaService);
+    mockReset(recordService);
+  });
+
+  describe('typecastRecordsWithField', () => {
+    it('should call castToSingleSelect for single select field', async () => {
+      const field = mockDeep<IFieldInstance>({ type: FieldType.SingleSelect, isComputed: false });
+      const typeCastAndValidate = new TypeCastAndValidate({ services, field, tableId });
+      const records: Record<string, unknown>[] = [];
+
+      jest.spyOn(typeCastAndValidate as any, 'castToSingleSelect').mockResolvedValue(records);
+
+      const result = await typeCastAndValidate.typecastRecordsWithField(records);
+
+      expect(result).toEqual(records);
+      expect(typeCastAndValidate['castToSingleSelect']).toBeCalledWith(records, FieldKeyType.Name);
+    });
+
+    it('should call castToMultipleSelect for multiple select field', async () => {
+      const field = mockDeep<IFieldInstance>({ type: FieldType.MultipleSelect, isComputed: false });
+      const typeCastAndValidate = new TypeCastAndValidate({ services, field, tableId });
+      const records: Record<string, unknown>[] = [];
+
+      jest.spyOn(typeCastAndValidate as any, 'castToMultipleSelect').mockResolvedValue(records);
+
+      const result = await typeCastAndValidate.typecastRecordsWithField(records);
+
+      expect(result).toEqual(records);
+      expect(typeCastAndValidate['castToMultipleSelect']).toBeCalledWith(
+        records,
+        FieldKeyType.Name
+      );
+    });
+
+    it('should call castToLink for link field', async () => {
+      const field = mockDeep<IFieldInstance>({ type: FieldType.Link, isComputed: false });
+      const typeCastAndValidate = new TypeCastAndValidate({ services, field, tableId });
+      const records: Record<string, unknown>[] = [];
+
+      jest.spyOn(typeCastAndValidate as any, 'castToLink').mockResolvedValue(records);
+
+      const result = await typeCastAndValidate.typecastRecordsWithField(records);
+
+      expect(result).toEqual(records);
+      expect(typeCastAndValidate['castToLink']).toBeCalledWith(records, FieldKeyType.Name);
+    });
+
+    it('should call defaultCastTo for other field', async () => {
+      const field = mockDeep<IFieldInstance>({ type: FieldType.SingleLineText, isComputed: false });
+      const typeCastAndValidate = new TypeCastAndValidate({ services, field, tableId });
+      const records: Record<string, unknown>[] = [];
+
+      jest.spyOn(typeCastAndValidate as any, 'defaultCastTo').mockResolvedValue(records);
+
+      const result = await typeCastAndValidate.typecastRecordsWithField(records);
+
+      expect(result).toEqual(records);
+      expect(typeCastAndValidate['defaultCastTo']).toBeCalledWith(records, FieldKeyType.Name);
+    });
+
+    it('should reject if sub method throws error', async () => {
+      const field = mockDeep<IFieldInstance>({ type: FieldType.SingleSelect, isComputed: false });
+      const typeCastAndValidate = new TypeCastAndValidate({ services, field, tableId });
+
+      jest.spyOn(typeCastAndValidate as any, 'castToSingleSelect').mockImplementation(() => {
+        throw new Error('xxxxx');
+      });
+
+      await expect(typeCastAndValidate.typecastRecordsWithField([])).rejects.toThrow();
+    });
+  });
+
+  describe('valueToStringArray', () => {
+    const typeCastAndValidate = new TypeCastAndValidate({
+      services,
+      field: mockDeep<IFieldInstance>(),
+      tableId,
+    });
+    it('should return null for null value', () => {
+      const result = typeCastAndValidate['valueToStringArray'](null);
+      expect(result).toBeNull();
+    });
+
+    it('should convert array to string array', () => {
+      const value = [1, '2', null, undefined];
+      const result = typeCastAndValidate['valueToStringArray'](value);
+      expect(result).toEqual(['1', '2']);
+    });
+
+    it('should return single element array for string', () => {
+      const value = 'str';
+      const result = typeCastAndValidate['valueToStringArray'](value);
+      expect(result).toEqual(['str']);
+    });
+
+    it('should convert object to string', () => {
+      const value = { toString: () => 'obj' };
+      const result = typeCastAndValidate['valueToStringArray'](value);
+      expect(result).toEqual(['obj']);
+    });
+
+    it('should filter out null values in array', () => {
+      const value = [1, null, 2];
+      const result = typeCastAndValidate['valueToStringArray'](value);
+      expect(result).toEqual(['1', '2']);
+    });
+
+    it('should filter out empty string values', () => {
+      const value = ['1', '', '2'];
+      const result = typeCastAndValidate['valueToStringArray'](value);
+      expect(result).toEqual(['1', '2']);
+    });
+
+    it('should handle error when toString throws', () => {
+      const value = {
+        toString: () => {
+          throw new Error();
+        },
+      };
+      expect(() => typeCastAndValidate['valueToStringArray'](value)).toThrow();
+    });
+  });
+
+  describe('mapFieldsRecordsWithValidate', () => {
+    const field = mockDeep<IFieldInstance>({ id: 'fldxxxx' });
+    const typeCastAndValidate = new TypeCastAndValidate({
+      services,
+      field,
+      tableId,
+      typecast: true,
+    });
+    it('should map record and apply callback', () => {
+      const records = [{ [field.id]: 1 }];
+      const callback = jest.fn(() => 'value');
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore-next-line
+      field.validateCellValue.mockReturnValue({
+        success: false,
+        error: 'error',
+      });
+
+      const result = typeCastAndValidate['mapFieldsRecordsWithValidate'](
+        records,
+        FieldKeyType.Id,
+        callback
+      );
+
+      expect(result).toEqual([{ [field.id]: 'value' }]);
+      expect(callback).toBeCalledWith(1);
+    });
+
+    it('should throw error when validate fails', () => {
+      const records = [{ [field.id]: 1 }];
+
+      const typeCastAndValidate = new TypeCastAndValidate({
+        services,
+        field,
+        tableId,
+      });
+
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore-next-line
+      field.validateCellValue.mockReturnValue({
+        success: false,
+        error: 'error',
+      });
+
+      expect(() => {
+        typeCastAndValidate['mapFieldsRecordsWithValidate'](records, FieldKeyType.Id, jest.fn());
+      }).toThrow('Bad Request');
+    });
+
+    it('should return original record if typecast is false', () => {
+      const typeCastAndValidate = new TypeCastAndValidate({
+        services,
+        field: mockDeep<IFieldInstance>(),
+        tableId,
+      });
+
+      field.validateCellValue.mockReturnValue({
+        success: true,
+      });
+
+      const records = [{ [field.id]: 1 }];
+
+      const result = typeCastAndValidate['mapFieldsRecordsWithValidate'](
+        records,
+        FieldKeyType.Id,
+        () => 'value'
+      );
+
+      expect(result).toEqual(records);
+    });
+
+    it('should not throw error if no field value', () => {
+      const records = [{ other: 1 }];
+
+      field.validateCellValue.mockReturnValue({
+        success: true,
+      });
+
+      const result = typeCastAndValidate['mapFieldsRecordsWithValidate'](
+        records,
+        FieldKeyType.Id,
+        jest.fn()
+      );
+
+      expect(result).toEqual(records);
+    });
+  });
+
+  describe('createOptionsIfNotExists', () => {
+    const field = {
+      id: 'fldxxxx',
+      type: FieldType.SingleSelect,
+      options: { choices: [{ id: 'xxx', name: '1', color: Colors.Blue }] },
+    } as any;
+    const typeCastAndValidate = new TypeCastAndValidate({
+      services,
+      field,
+      tableId,
+      typecast: true,
+    });
+
+    it('should create new options and update field', async () => {
+      fieldConvertingService.updateFieldById.mockImplementation();
+
+      await typeCastAndValidate['createOptionsIfNotExists'](['1', '2']);
+
+      expect(fieldConvertingService.updateFieldById).toBeCalledWith(
+        tableId,
+        field.id,
+        expect.objectContaining({
+          type: FieldType.SingleSelect,
+          options: expect.objectContaining({
+            choices: expect.arrayContaining([
+              expect.objectContaining({ name: '1' }),
+              expect.objectContaining({ name: '2' }),
+            ]),
+          }),
+        })
+      );
+    });
+
+    it('should return if no options', async () => {
+      fieldConvertingService.updateFieldById.mockImplementation();
+      await typeCastAndValidate['createOptionsIfNotExists']([]);
+
+      expect(fieldConvertingService.updateFieldById).not.toBeCalled();
+    });
+  });
+
+  describe('defaultCastTo', () => {
+    it('should call mapFieldsRecordsWithValidate with repair callback', () => {
+      const field = mockDeep<IFieldInstance>({ id: 'fldxxxx', repair: () => 'repair' });
+      const records = [{ [field.id]: 'value' }];
+      const typeCastAndValidate = new TypeCastAndValidate({
+        services,
+        field,
+        tableId,
+        typecast: true,
+      });
+
+      jest
+        .spyOn(typeCastAndValidate as any, 'mapFieldsRecordsWithValidate')
+        .mockImplementation((...args) => (args[2] as any)());
+
+      const result = typeCastAndValidate['defaultCastTo'](records, FieldKeyType.Id);
+
+      expect(result).toEqual('repair');
+    });
+  });
+
+  describe('castToSingleSelect', () => {
+    const field = mockDeep<SingleSelectFieldDto>({
+      id: 'fldxxxx',
+      type: FieldType.SingleSelect,
+      options: { choices: [{ id: '1', name: 'option 1', color: Colors.Blue }] },
+    });
+    const records = [{ [field.id]: 'value' }];
+    const typeCastAndValidate = new TypeCastAndValidate({
+      services,
+      field,
+      tableId,
+      typecast: true,
+    });
+    it('should call dependencies correctly and return', async () => {
+      jest
+        .spyOn(typeCastAndValidate as any, 'mapFieldsRecordsWithValidate')
+        .mockImplementation((...args) => (args[2] as any)('value'));
+
+      jest.spyOn(typeCastAndValidate as any, 'createOptionsIfNotExists').mockImplementation();
+
+      const result = await typeCastAndValidate['castToSingleSelect'](records, FieldKeyType.Id);
+
+      expect(typeCastAndValidate['mapFieldsRecordsWithValidate']).toBeCalled();
+      expect(typeCastAndValidate['createOptionsIfNotExists']).toBeCalledWith(['value']);
+      expect(result).toEqual('value');
+    });
+  });
+
+  describe('castToMultipleSelect', () => {
+    const field = mockDeep<SingleSelectFieldDto>({
+      id: 'fldxxxx',
+      type: FieldType.SingleSelect,
+      options: { choices: [{ id: '1', name: 'option 1', color: Colors.Blue }] },
+    });
+    const records = [{ [field.id]: 'value' }];
+    const typeCastAndValidate = new TypeCastAndValidate({
+      services,
+      field,
+      tableId,
+      typecast: true,
+    });
+    it('should call dependencies correctly and return', async () => {
+      jest
+        .spyOn(typeCastAndValidate as any, 'mapFieldsRecordsWithValidate')
+        .mockImplementation((...args) => (args[2] as any)('value'));
+
+      jest.spyOn(typeCastAndValidate as any, 'createOptionsIfNotExists').mockImplementation();
+
+      const result = await typeCastAndValidate['castToMultipleSelect'](records, FieldKeyType.Id);
+
+      expect(typeCastAndValidate['mapFieldsRecordsWithValidate']).toBeCalled();
+      expect(typeCastAndValidate['createOptionsIfNotExists']).toBeCalledWith(['value']);
+      expect(result).toEqual(['value']);
+    });
+  });
+
+  describe('getLinkTableRecordMap', () => {
+    const field = mockDeep<LinkFieldDto>({
+      id: 'fldxxxx',
+      type: FieldType.Link,
+      options: { foreignTableId: 'foreignTableId' },
+    });
+    const typeCastAndValidate = new TypeCastAndValidate({
+      services,
+      field,
+      tableId,
+      typecast: true,
+    });
+    it('should call dependencies correctly and return recordMap', async () => {
+      recordService.getRecordsWithPrimary.mockResolvedValue([{ id: '1', title: 'title1' }]);
+
+      const result = await typeCastAndValidate['getLinkTableRecordMap']();
+
+      expect(recordService.getRecordsWithPrimary).toBeCalledWith('foreignTableId');
+      expect(result).toEqual({
+        title1: '1',
+      });
+    });
+  });
+
+  describe('castToLinkOne', () => {
+    const typeCastAndValidate = new TypeCastAndValidate({
+      services,
+      field: mockDeep<IFieldInstance>(),
+      tableId,
+      typecast: true,
+    });
+
+    it('should cast value correctly and return one linkCellValue', () => {
+      typeCastAndValidate['field'].isMultipleCellValue = true;
+      const result = typeCastAndValidate['castToLinkOne'](['a', 'b', 'c'], { a: '1', b: '2' });
+
+      expect(result).toEqual([
+        { title: 'a', id: '1' },
+        { title: 'b', id: '2' },
+      ]);
+    });
+
+    it('should cast value correctly and return multipleCellValue linkCellValue', () => {
+      typeCastAndValidate['field'].isMultipleCellValue = false;
+      const result = typeCastAndValidate['castToLinkOne'](['a', 'b', 'c'], { a: '1', b: '2' });
+
+      expect(result).toEqual({ title: 'a', id: '1' });
+    });
+  });
+
+  describe('castToLink', () => {
+    const field = mockDeep<LinkFieldDto>();
+    const records = [{ [field.id]: 'value' }];
+    const typeCastAndValidate = new TypeCastAndValidate({
+      services,
+      field,
+      tableId,
+      typecast: true,
+    });
+    it('should call dependencies correctly and return map by typecast', async () => {
+      jest.spyOn(typeCastAndValidate as any, 'getLinkTableRecordMap').mockResolvedValue({});
+
+      jest
+        .spyOn(typeCastAndValidate as any, 'mapFieldsRecordsWithValidate')
+        .mockImplementation((...args) => (args[2] as any)('title'));
+
+      jest.spyOn(typeCastAndValidate as any, 'castToLinkOne').mockReturnValue({ title1: '1' });
+
+      const result = await typeCastAndValidate['castToLink'](records, FieldKeyType.Id);
+
+      expect(result).toEqual({ title1: '1' });
+    });
+  });
+});
---
+++ apps/nestjs-backend/src/features/record/typecast.validate.ts
@@ -0,0 +1,257 @@
+import { BadRequestException } from '@nestjs/common';
+import type { ILinkCellValue } from '@teable-group/core';
+import { ColorUtils, FieldKeyType, FieldType, generateChoiceId } from '@teable-group/core';
+import type { PrismaService } from '@teable-group/db-main-prisma';
+import { has, isUndefined, keyBy, map } from 'lodash';
+import { fromZodError } from 'zod-validation-error';
+import type { FieldConvertingService } from '../field/field-calculate/field-converting.service';
+import type { IFieldInstance } from '../field/model/factory';
+import type { LinkFieldDto } from '../field/model/field-dto/link-field.dto';
+import type { MultipleSelectFieldDto } from '../field/model/field-dto/multiple-select-field.dto';
+import type { SingleSelectFieldDto } from '../field/model/field-dto/single-select-field.dto';
+import type { RecordService } from './record.service';
+
+interface IServices {
+  prismaService: PrismaService;
+  fieldConvertingService: FieldConvertingService;
+  recordService: RecordService;
+}
+
+/**
+ * Cell type conversion:
+ * Because there are some merge operations, we choose column-by-column conversion here.
+ */
+export class TypeCastAndValidate {
+  private readonly services: IServices;
+  private readonly field: IFieldInstance;
+  private readonly tableId: string;
+  private readonly typecast?: boolean;
+
+  constructor({
+    services,
+    field,
+    typecast,
+    tableId,
+  }: {
+    services: IServices;
+    field: IFieldInstance;
+    typecast?: boolean;
+    tableId: string;
+  }) {
+    this.services = services;
+    this.field = field;
+    this.typecast = typecast;
+    this.tableId = tableId;
+  }
+
+  /**
+   * Attempts to cast a cell value to the appropriate type based on the field configuration.
+   * Calls the appropriate typecasting method depending on the field type.
+   */
+  async typecastRecordsWithField(
+    fieldsRecords: Record<string, unknown>[],
+    fieldKeyType: FieldKeyType = FieldKeyType.Name
+  ) {
+    const { type, isComputed } = this.field;
+    if (isComputed) {
+      return fieldsRecords;
+    }
+    switch (type) {
+      case FieldType.SingleSelect:
+        return await this.castToSingleSelect(fieldsRecords, fieldKeyType);
+      case FieldType.MultipleSelect:
+        return await this.castToMultipleSelect(fieldsRecords, fieldKeyType);
+      case FieldType.Link: {
+        return await this.castToLink(fieldsRecords, fieldKeyType);
+      }
+      default:
+        return this.defaultCastTo(fieldsRecords, fieldKeyType);
+    }
+  }
+
+  private defaultCastTo(fieldsRecords: Record<string, unknown>[], fieldKeyType: FieldKeyType) {
+    return this.mapFieldsRecordsWithValidate(fieldsRecords, fieldKeyType, (cellValue: unknown) => {
+      return this.field.repair(cellValue);
+    });
+  }
+
+  /**
+   * Traverse fieldRecords, and do validation here.
+   */
+  private mapFieldsRecordsWithValidate(
+    fieldsRecords: Record<string, unknown>[],
+    fieldKeyType: FieldKeyType,
+    callBack: (cellValue: unknown) => unknown
+  ) {
+    return fieldsRecords.map((record) => {
+      const fieldIdOrName = this.field[fieldKeyType];
+      if (!has(record, fieldIdOrName)) {
+        return record;
+      }
+      const cellValue = record[fieldIdOrName];
+      const validate = this.field.validateCellValue(cellValue);
+      if (!validate.success) {
+        if (this.typecast) {
+          const typecastCv = callBack(cellValue);
+          return {
+            ...record,
+            [fieldIdOrName]: typecastCv,
+          };
+        } else {
+          throw new BadRequestException(fromZodError(validate.error).message);
+        }
+      }
+      return record;
+    });
+  }
+
+  /**
+   * Converts the provided value to a string array.
+   * Handles multiple types of input such as arrays, strings, and other types.
+   */
+  private valueToStringArray(value: unknown): string[] | null {
+    if (value == null) {
+      return null;
+    }
+    if (Array.isArray(value)) {
+      return value.filter((v) => v != null && v !== '').map(String);
+    }
+    if (typeof value === 'string') {
+      return [value];
+    }
+    const strValue = String(value);
+    if (strValue != null) {
+      return [String(value)];
+    }
+    return null;
+  }
+
+  /**
+   * Creates select options if they do not already exist in the field.
+   * Also updates the field with the newly created options.
+   */
+  private async createOptionsIfNotExists(choicesNames: string[]) {
+    if (!choicesNames.length) {
+      return;
+    }
+    const { id, type, options } = this.field as SingleSelectFieldDto | MultipleSelectFieldDto;
+    const existsChoicesNameMap = keyBy(options.choices, 'name');
+    const notExists = choicesNames.filter((name) => !existsChoicesNameMap[name]);
+    const colors = ColorUtils.randomColor(map(options.choices, 'color'), notExists.length);
+    const newChoices = notExists.map((name, index) => ({
+      id: generateChoiceId(),
+      name,
+      color: colors[index],
+    }));
+
+    await this.services.fieldConvertingService.updateFieldById(this.tableId, id, {
+      type,
+      options: {
+        ...options,
+        choices: options.choices.concat(newChoices),
+      },
+    });
+  }
+
+  /**
+   * Casts the value to a single select option.
+   * Creates the option if it does not already exist.
+   */
+  private async castToSingleSelect(
+    fieldsRecords: Record<string, unknown>[],
+    fieldKeyType: FieldKeyType
+  ): Promise<Record<string, unknown>[]> {
+    const allValuesSet = new Set<string>();
+    const newFieldsRecords = this.mapFieldsRecordsWithValidate(
+      fieldsRecords,
+      fieldKeyType,
+      (cellValue: unknown) => {
+        const valueArr = this.valueToStringArray(cellValue);
+        const newCellValue: string | null = valueArr?.length ? valueArr[0] : null;
+        newCellValue && allValuesSet.add(newCellValue);
+        return newCellValue;
+      }
+    );
+    await this.createOptionsIfNotExists([...allValuesSet]);
+    return newFieldsRecords;
+  }
+
+  /**
+   * Casts the value to multiple select options.
+   * Creates the option if it does not already exist.
+   */
+  private async castToMultipleSelect(
+    fieldsRecords: Record<string, unknown>[],
+    fieldKeyType: FieldKeyType
+  ): Promise<Record<string, unknown>[]> {
+    const allValuesSet = new Set<string>();
+    const newFieldsRecords = this.mapFieldsRecordsWithValidate(
+      fieldsRecords,
+      fieldKeyType,
+      (cellValue: unknown) => {
+        const valueArr = this.valueToStringArray(cellValue);
+        const newCellValue: string[] | null = valueArr?.length ? valueArr : null;
+        // collect all options
+        newCellValue?.forEach((v) => v && allValuesSet.add(v));
+        return newCellValue;
+      }
+    );
+    await this.createOptionsIfNotExists([...allValuesSet]);
+    return newFieldsRecords;
+  }
+
+  /**
+   * Casts the value to a link type, associating it with another table.
+   * Try to find the rows with matching titles from the associated table and write them to the cell.
+   */
+  private async castToLink(
+    fieldsRecords: Record<string, unknown>[],
+    fieldKeyType: FieldKeyType
+  ): Promise<Record<string, unknown>[]> {
+    const linkRecordMap = this.typecast ? await this.getLinkTableRecordMap() : {};
+
+    return this.mapFieldsRecordsWithValidate(fieldsRecords, fieldKeyType, (cellValue: unknown) => {
+      const newCellValue: ILinkCellValue[] | ILinkCellValue | null = this.castToLinkOne(
+        cellValue,
+        linkRecordMap
+      );
+      return newCellValue;
+    });
+  }
+
+  /**
+   * Get the recordMap of the associated table, the format is: {[title]: [id]}.
+   */
+  private async getLinkTableRecordMap() {
+    const linkRecords = await this.services.recordService.getRecordsWithPrimary(
+      (this.field as LinkFieldDto).options.foreignTableId
+    );
+    return linkRecords.reduce((result, { id, title }) => {
+      if (!result[title]) {
+        result[title] = id;
+      }
+      return result;
+    }, {} as Record<string, string>);
+  }
+
+  /**
+   * The conversion of cellValue here is mainly about the difference between filtering null values,
+   * returning data based on isMultipleCellValue.
+   */
+  private castToLinkOne(
+    value: unknown,
+    linkTableRecordMap: Record<string, string>
+  ): ILinkCellValue[] | ILinkCellValue | null {
+    const { isMultipleCellValue } = this.field;
+    let valueArr = this.valueToStringArray(value);
+    if (!valueArr?.length) {
+      return null;
+    }
+    valueArr = isMultipleCellValue ? valueArr : valueArr.slice(0, 1);
+    const valueArrNotEmpty = valueArr.map(String).filter((v) => v !== undefined || v !== '');
+    const result = valueArrNotEmpty
+      .map((v) => ({ title: v, id: linkTableRecordMap[v] }))
+      .filter((v) => !isUndefined(v.id)) as ILinkCellValue[];
+    return isMultipleCellValue ? result : result[0] ?? null;
+  }
+}
