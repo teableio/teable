4d7234567b46dab41a0fefa16867293f451f6f7a
Boris Bo Wang <boris2code@outlook.com>
2023-10-26 20:39:10 +0800

    fix: attachment upload (#225)
    * fix: attachment upload
    
    * feat: improved file upload drag and drop experience

--- packages/sdk/src/components/editor/attachment/upload-attachment/DragAndCopy.tsx
+++ packages/sdk/src/components/editor/attachment/upload-attachment/DragAndCopy.tsx
@@ -1,45 +1,24 @@
 import classNames from 'classnames';
-import { useState } from 'react';
+import { useDropArea } from 'react-use';
 
-export const DragAndCopy = (props: {
-  onChange?: (files: FileList) => void;
-  disabled?: boolean;
-}) => {
+export const DragAndCopy = (props: { onChange?: (files: File[]) => void; disabled?: boolean }) => {
   const { onChange, disabled } = props;
-  const [isFileDragIn, setIsFileDragIn] = useState(false);
 
-  const handleFileDrop = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    const files = e.dataTransfer.files;
-    uploadFiles(files);
-  };
-
-  const handleFilePaste = (e: React.ClipboardEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    const clipboardData = e.clipboardData;
-    uploadFiles(clipboardData.files);
-  };
-
-  const uploadFiles = (files: FileList) => {
-    if (files.length === 0) return;
-    onChange?.(files);
-  };
+  const [bound, { over }] = useDropArea({
+    onFiles: onChange,
+  });
 
   return (
-    <div className="w-full h-full min-h-[64px] flex flex-col focus:bg-red">
+    <div className="w-full h-full flex flex-col">
       <div
         className={classNames(
           'flex-1 w-full bg-foreground/5 text-foreground/60 rounded-md flex items-center justify-center border border-dashed',
-          isFileDragIn && 'border-foreground',
+          over && 'border-foreground',
           disabled && 'opacity-50 cursor-not-allowed'
         )}
-        onDrop={handleFileDrop}
-        onDragEnter={() => setIsFileDragIn(true)}
-        onDragLeave={() => setIsFileDragIn(false)}
-        onDragOver={(e) => e.preventDefault()}
-        onPaste={handleFilePaste}
+        {...bound}
       >
-        {isFileDragIn ? 'Release to upload file.' : 'Paste or drag and drop to upload here.'}
+        {over ? 'Release to upload file.' : 'Paste or drag and drop to upload here.'}
       </div>
     </div>
   );
