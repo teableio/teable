826394d5baef797a2ece77dedd852fc036fb208e
xing_cx <caoxing9@gmail.com>
2023-11-01 20:31:13 +0800

    feat: optimise logo display in dark mode

--- apps/nextjs-app/src/features/app/blocks/space/space-side-bar/SideBarHeader.tsx
+++ apps/nextjs-app/src/features/app/blocks/space/space-side-bar/SideBarHeader.tsx
@@ -7,7 +7,7 @@ export const SideBarHeader: React.FC = () => {
 
   return (
     <div className="m-2 flex items-center gap-1">
-      <TeableNew className="h-6 w-6 shrink-0" />
+      <TeableNew className="h-6 w-6 shrink-0 text-black" />
       <p className="truncate text-sm">Teable</p>
       <div className="grow basis-0"></div>
       {isHydrated && <ThemePicker className="px-1" />}
