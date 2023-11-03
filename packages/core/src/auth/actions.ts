e7f3beecdd215fa375078fd8fc3fc22fa608a5fd
boris <boris2code@outlook.com>
2023-11-03 15:16:18 +0800

    feat: add permission checks for socket resource operations

--- packages/core/src/auth/actions.ts
+++ packages/core/src/auth/actions.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import { z } from 'zod';
 
-enum IdPrefix {
+export enum ActionPrefix {
   Space = 'space',
   Base = 'base',
   Table = 'table',
@@ -10,32 +10,30 @@ enum IdPrefix {
   Field = 'field',
 }
 
-export { IdPrefix as PermissionIdPrefix };
-
 const defaultActionsSchema = z.enum(['create', 'update', 'delete', 'read']);
 
 export const spaceActionsSchema = defaultActionsSchema.or(
   z.enum(['invite_email', 'invite_link', 'grant_role'])
 );
 
-export type SpaceActions = `${IdPrefix.Space}|${z.infer<typeof spaceActionsSchema>}`;
+export type SpaceActions = `${ActionPrefix.Space}|${z.infer<typeof spaceActionsSchema>}`;
 
 export const baseActionsSchema = defaultActionsSchema.or(z.enum(['invite_email', 'invite_link']));
 
-export type BaseActions = `${IdPrefix.Base}|${z.infer<typeof baseActionsSchema>}`;
+export type BaseActions = `${ActionPrefix.Base}|${z.infer<typeof baseActionsSchema>}`;
 
 export const tableActionsSchema = defaultActionsSchema.or(z.enum(['import']));
 
-export type TableActions = `${IdPrefix.Table}|${z.infer<typeof tableActionsSchema>}`;
+export type TableActions = `${ActionPrefix.Table}|${z.infer<typeof tableActionsSchema>}`;
 
 export const viewActionsSchema = defaultActionsSchema;
 
-export type ViewActions = `${IdPrefix.View}|${z.infer<typeof viewActionsSchema>}`;
+export type ViewActions = `${ActionPrefix.View}|${z.infer<typeof viewActionsSchema>}`;
 
 export const fieldActionsSchema = defaultActionsSchema;
 
-export type FieldActions = `${IdPrefix.Field}|${z.infer<typeof fieldActionsSchema>}`;
+export type FieldActions = `${ActionPrefix.Field}|${z.infer<typeof fieldActionsSchema>}`;
 
 export const recordActionsSchema = defaultActionsSchema.or(z.enum(['comment']));
 
-export type RecordActions = `${IdPrefix.Record}|${z.infer<typeof recordActionsSchema>}`;
+export type RecordActions = `${ActionPrefix.Record}|${z.infer<typeof recordActionsSchema>}`;
