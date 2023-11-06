23199ab5039fe52217244e411d43bcfb044fe5da
Boris Bo Wang <boris2code@outlook.com>
2023-11-06 11:14:05 +0800

    feat: add permission checks for socket resource operations (#245)

--- apps/nestjs-backend/src/share-db/share-db.module.ts
+++ apps/nestjs-backend/src/share-db/share-db.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../features/auth/auth.module';
+import { PermissionService } from '../features/auth/permission.service';
 import { CalculationModule } from '../features/calculation/calculation.module';
 import { TableModule } from '../features/table/table.module';
 import { UserModule } from '../features/user/user.module';
@@ -10,7 +11,7 @@ import { WsDerivateService } from './ws-derivate.service';
 
 @Module({
   imports: [TableModule, CalculationModule, AuthModule, UserModule],
-  providers: [ShareDbService, SqliteDbAdapter, WsDerivateService, WsAuthService],
+  providers: [ShareDbService, SqliteDbAdapter, WsDerivateService, WsAuthService, PermissionService],
   exports: [ShareDbService, WsAuthService],
 })
 export class ShareDbModule {}
