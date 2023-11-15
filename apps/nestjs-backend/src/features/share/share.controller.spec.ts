3c9182605422b279855a09951125a702c6ae49d9
boris <boris2code@outlook.com>
2023-11-15 14:49:53 +0800

    feat: supplementary share-related e2e

--- apps/nestjs-backend/src/features/share/share.controller.spec.ts
+++ apps/nestjs-backend/src/features/share/share.controller.spec.ts
@@ -1,4 +1,5 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import type { TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { ShareController } from './share.controller';
 
 describe('ShareController', () => {
--- apps/nestjs-backend/src/features/share/share.service.spec.ts
+++ apps/nestjs-backend/src/features/share/share.service.spec.ts
@@ -1,4 +1,5 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import type { TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { ShareService } from './share.service';
 
 describe('ShareService', () => {
