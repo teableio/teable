8514016b1ed548372d7d0b930db4ac37c91e137e
caoxing <caoxing9@gmail.com>
2023-11-23 19:38:05 +0800

    fix: update nestjs build config and fix clipboard bug (#261)
    * fix: remove references from tsconfig in nestjs
    
    * fix: table header colspan bug in clipboard serializer
    
    * build: replace reference to paths in openapi
    
    * build: distinguish nestjs webpack config with dev cache optimise
    
    * build: package openapi use tsconfig.build.json to build and typecheck

--- apps/nestjs-backend/webpack.config.js
+++ apps/nestjs-backend/webpack.config.js
@@ -1,28 +1,5 @@
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const nodeExternals = require('webpack-node-externals');
-
-module.exports = function (options, webpack) {
+module.exports = function (options) {
   return {
     ...options,
-    entry: ['webpack/hot/poll?100', options.entry],
-    externals: [
-      nodeExternals({
-        allowlist: ['webpack/hot/poll?100', /^@teable-group/],
-      }),
-    ],
-    module: {
-      rules: [
-        {
-          test: /\.ts?$/,
-          loader: 'ts-loader',
-          exclude: /node_modules/,
-        },
-      ],
-    },
-    plugins: [
-      ...options.plugins,
-      new webpack.HotModuleReplacementPlugin(),
-      new ForkTsCheckerWebpackPlugin(),
-    ],
   };
 };
