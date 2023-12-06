f58c83d2e86af91dd656133ea50bb944c2749f9c
tea artist <artist@teable.io>
2023-12-06 15:11:35 +0800

    perf: skip unnecessary calculation

--- apps/nestjs-backend/src/features/field/field-calculate/field-converting.service.ts
+++ apps/nestjs-backend/src/features/field/field-calculate/field-converting.service.ts
@@ -882,8 +882,9 @@ export class FieldConvertingService {
     console.log('changed Keys:' + JSON.stringify(keys));
 
     let result: IModifiedResult | undefined;
-    // for field type change, isLookup change, isComputed change
+    // 0.1. collect changes effect by the updated field
     if (keys.includes('type') || keys.includes('isComputed')) {
+      // for field type change, isLookup change, isComputed change
       result = await this.modifyType(tableId, newField, oldField);
     } else {
       // for same field with options change
@@ -897,24 +898,26 @@ export class FieldConvertingService {
       }
     }
 
-    const fieldChange = await this.fieldConvertingLinkService.supplementLink(
+    // 0.2. collect changes effect by the supplement(link) field
+    const supplementFieldChange = await this.fieldConvertingLinkService.supplementLink(
       tableId,
       newField,
       oldField
     );
 
+    // 1. apply current field changes
     await this.fieldService.batchUpdateFields(tableId, [
       { fieldId: newField.id, ops: ops.concat(result?.fieldOps || []) },
     ]);
 
-    // apply supplement(link) field change
-    if (fieldChange) {
-      const { tableId, newField, oldField } = fieldChange;
+    // 2. apply supplement(link) field changes
+    if (supplementFieldChange) {
+      const { tableId, newField, oldField } = supplementFieldChange;
       const { ops } = this.getOriginFieldOps(newField, oldField);
       await this.fieldService.batchUpdateFields(tableId, [{ fieldId: newField.id, ops }]);
     }
 
-    // create and submit records
+    // 3. apply create records changes
     if (result?.recordsForCreate) {
       for (const tableId in result.recordsForCreate) {
         const recordsMap = result.recordsForCreate[tableId];
@@ -926,20 +929,50 @@ export class FieldConvertingService {
       }
     }
 
-    // update & submit referenced fields
+    // 4. apply referenced fields changes
     await this.updateReferencedFields(newField, oldField);
-    if (fieldChange) {
-      const { newField, oldField } = fieldChange;
+
+    // 5. apply referenced fields from supplement(link) field changes
+    if (supplementFieldChange) {
+      const { newField, oldField } = supplementFieldChange;
       await this.updateReferencedFields(newField, oldField);
     }
 
-    // calculate and submit records
+    // 6. calculate and submit records
     await this.calculateAndSaveRecords(tableId, newField, result?.recordOpsMap);
 
-    // TODO: reduce unnecessary calculation
-    if (newField.isComputed) {
-      await this.fieldCalculationService.calculateFields(tableId, [newField.id]);
+    // 7. calculate computed fields
+    await this.calculateField(keys, tableId, newField, oldField);
+  }
+
+  private async calculateField(
+    keys: string[],
+    tableId: string,
+    newField: IFieldInstance,
+    oldField: IFieldInstance
+  ) {
+    if (!newField.isComputed) {
+      return;
     }
+    // safe property
+    const differenceKeys = difference(['name', 'description', 'dbFieldName'], keys);
+
+    if (!differenceKeys.length) {
+      return;
+    }
+
+    // expression not change
+    if (
+      differenceKeys.length === 1 &&
+      differenceKeys[0] === 'options' &&
+      (oldField.options as { expression: string }).expression ===
+        (newField.options as { expression: string }).expression
+    ) {
+      return;
+    }
+
+    console.log('calculating field:', newField.name);
+    await this.fieldCalculationService.calculateFields(tableId, [newField.id]);
   }
 
   private async submitFieldOpsMap(fieldOpsMap: IOpsMap | undefined) {
