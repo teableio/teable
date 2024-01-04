8af26136c3584a5a7f22dc0e97ea5726c9faeb13
xing_cx <caoxing9@gmail.com>
2024-01-04 15:11:04 +0800

    perf: optimise hover sider bar animation

--- apps/nextjs-app/src/features/app/blocks/base/base-side-bar/HoverWraper.tsx
+++ apps/nextjs-app/src/features/app/blocks/base/base-side-bar/HoverWraper.tsx
@@ -14,34 +14,46 @@ export const HoverWraper = (props: IHoverWraperProps) => {
   const { children, size = 25 } = props;
   const [trigger, content] = children;
   const [hover, setHover] = useState(false);
+  const [contentVisible, setContentVisible] = useState(false);
 
   const mouseEnterHandler = () => {
     setHover(true);
+    setContentVisible(true);
   };
 
   const mouseOutHandler = () => {
     setHover(false);
+    setTimeout(() => {
+      setContentVisible(false);
+    }, 300);
   };
 
   return (
     <div>
-      <div onMouseEnter={() => mouseEnterHandler()} className="z-[20]">
+      <div onMouseEnter={() => mouseEnterHandler()} className="z-10">
         {trigger}
       </div>
-      <div className={classNames('fixed z-50 flex h-full top-0', hover ? 'w-full' : 'w-auto')}>
+      {
         <div
-          className={classNames('transition-[width] overflow-hidden drop-shadow-2xl border-r')}
-          style={{
-            width: hover ? `${size}%` : '0%',
-          }}
+          className={classNames(
+            'fixed flex h-full top-0',
+            hover ? 'z-50 w-full ' : contentVisible ? 'w-auto z-10' : 'w-auto z-0'
+          )}
         >
-          {content}
+          <div
+            className={classNames('transition-[width] overflow-hidden drop-shadow-2xl border-r')}
+            style={{
+              width: hover ? `${size}%` : '0%',
+            }}
+          >
+            {content}
+          </div>
+          <div
+            onMouseEnter={() => mouseOutHandler()}
+            className={classNames('flex-1', { hidden: !hover })}
+          ></div>
         </div>
-        <div
-          onMouseEnter={() => mouseOutHandler()}
-          className={classNames('flex-1', { hidden: !hover })}
-        ></div>
-      </div>
+      }
     </div>
   );
 };
