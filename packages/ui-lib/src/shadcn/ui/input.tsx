6f554aba831ea1e133651ec7d3cd597a30323ddb
tea artist <artist@teable.io>
2024-01-05 13:36:37 +0800

    chore: update shadcn/ui to latest

--- packages/ui-lib/src/shadcn/ui/input.tsx
+++ packages/ui-lib/src/shadcn/ui/input.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import { cn } from '../utils';
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
--- packages/ui-lib/src/shadcn/ui/textarea.tsx
+++ packages/ui-lib/src/shadcn/ui/textarea.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 import { cn } from '../utils';
 
-export type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
