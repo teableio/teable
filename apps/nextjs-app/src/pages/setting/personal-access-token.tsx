165822e184805b12edafb48138e60fe20d9bc805
pengap <penganpingprivte@gmail.com>
2024-01-30 16:49:36 +0800

    fix: import path

--- apps/nextjs-app/src/pages/setting/personal-access-token.tsx
+++ apps/nextjs-app/src/pages/setting/personal-access-token.tsx
@@ -4,7 +4,7 @@ import { PersonAccessTokenPage } from '@/features/app/blocks/setting/access-toke
 import { SettingLayout } from '@/features/app/layouts/SettingLayout';
 import { personalAccessTokenConfig } from '@/features/i18n/personal-access-token.config';
 import { getTranslationsProps } from '@/lib/i18n';
-import type { NextPageWithLayout } from '../type';
+import type { NextPageWithLayout } from '@/lib/type';
 
 const PersonalAccessToken: NextPageWithLayout = () => {
   return <PersonAccessTokenPage />;
