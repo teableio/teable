7398f6abdef013cb922bade9b059b419a23c0f30
xing_cx <caoxing9@gmail.com>
2024-02-19 17:54:19 +0800

    feat: delete sidebar skeleton and sidebar localstorage

--- packages/sdk/src/config/local-storage-keys.ts
+++ packages/sdk/src/config/local-storage-keys.ts
@@ -1,7 +1,6 @@
 export enum LocalStorageKeys {
   Theme = 'ls_theme',
   FieldSystem = 'ls_field_system',
-  SideBarSize = 'ls_side_bar_size',
   ShowActivity = 'ls_show_activity',
   ExpandRecordPanelSize = 'ls_expand_record_panel_size',
   DashboardKey = 'ls_dashboard_key',
