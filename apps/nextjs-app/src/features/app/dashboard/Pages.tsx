2f59cedb87cc630738eae4a77fdf50a2b21978e6
tea artist <artist@teable.io>
2024-02-22 20:37:58 +0800

    feat: add help tip

--- apps/nextjs-app/src/features/app/dashboard/Pages.tsx
+++ apps/nextjs-app/src/features/app/dashboard/Pages.tsx
@@ -1,10 +1,10 @@
 import { AnchorProvider } from '@teable/sdk/context';
 import { Button, Tabs } from '@teable/ui-lib/shadcn';
-import { Alert, AlertTitle, AlertDescription } from '@teable/ui-lib/shadcn/ui/alert';
 import { useTranslation } from 'next-i18next';
 import { useState } from 'react';
 import { useLocalStorage } from 'react-use';
 import { dashboardConfig } from '@/features/i18n/dashboard.config';
+import { getHelpLink } from '@/lib/help-link';
 import { Pickers } from './components/Pickers';
 import { GridContent } from './GridContent';
 
@@ -22,18 +22,33 @@ export function DashboardPage() {
             <h2 className="text-3xl font-bold tracking-tight">{t('common:noun.dashboard')}</h2>
           </div>
           {!showDashboard ? (
-            <div className="flex h-full items-center justify-center p-4">
-              <Alert className="w-[400px]">
-                <AlertTitle>
-                  <span className="text-lg">🏗️</span> Coming soon
-                </AlertTitle>
-                <AlertDescription>
-                  Dashboard is under development, click to view demo{' '}
-                  <Button size="xs" onClick={() => setShowDashboard(true)}>
-                    Demo
+            <div className="flex h-full flex-col items-center justify-center p-4">
+              <ul className="mb-4 space-y-2 text-left">
+                <li>Click the + sign on the left sidebar to create a table.</li>
+                <li>
+                  Visit the{' '}
+                  <a
+                    href={getHelpLink()}
+                    className="text-blue-500 hover:text-blue-700"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Help Center
+                  </a>{' '}
+                  for assistance.
+                </li>
+                <li>
+                  Dashboard is under development,
+                  <Button
+                    className="text-md"
+                    variant="link"
+                    size="xs"
+                    onClick={() => setShowDashboard(true)}
+                  >
+                    click to view demo
                   </Button>
-                </AlertDescription>
-              </Alert>
+                </li>
+              </ul>
             </div>
           ) : (
             <Tabs defaultValue="overview" className="overflow-y-auto">
