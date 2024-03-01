9db85203f3529b8988e419d5b936f9a4e1fbde65
boris <boris2code@outlook.com>
2024-03-01 16:47:05 +0800

    feat: archive business type http error code

--- packages/core/src/errors/http/http-response.types.ts
+++ packages/core/src/errors/http/http-response.types.ts
@@ -10,18 +10,22 @@ export type IHttpError = {
 };
 
 export enum HttpErrorCode {
-  INVALID_JSON = 'invalid_json',
-  INVALID_REQUEST_URL = 'invalid_request_url',
-  INVALID_REQUEST = 'invalid_request',
+  // 400 - The request body does not match the schema for the expected parameters
   VALIDATION_ERROR = 'validation_error',
-  MESSING_VERSION = 'missing_version',
+  // 403 - The bearer token is not valid.
   UNAUTHORIZED = 'unauthorized',
+  // 403 - Given the bearer token used, the client doesn't have permission to perform this operation.
   UNAUTHORIZED_SHARE = 'unauthorized_share',
+  // 403 - Given the bearer token used, the client doesn't have permission to perform this operation.
   RESTRICTED_RESOURCE = 'restricted_resource',
+  // 404 - Given the bearer token used, the resource does not exist. This error can also indicate that the resource has not been shared with owner of the bearer token.
   NOT_FOUND = 'not_found',
-  CONFLICT_ERROR = 'conflict_error',
+  // 500 - An unexpected error occurred.
   INTERNAL_SERVER_ERROR = 'internal_server_error',
-  SERVICE_UNAVAILABLE = 'service_unavailable',
+  // 503 - database is unavailable or is not in a state that can be queried. Please try again later.
   DATABASE_CONNECTION_UNAVAILABLE = 'database_connection_unavailable',
+  // 504 - The server, while acting as a gateway or proxy, did not receive a timely response from the upstream server it needed to access in order to complete the request.
   GATEWAY_TIMEOUT = 'gateway_timeout',
+  // Unknown error code
+  UNKNOWN_ERROR_CODE = 'unknown_error_code',
 }
