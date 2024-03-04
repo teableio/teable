8fc1be9eb536cb9e37f97dc36d2e5084a54eb7f8
SkyHuang <906268297@qq.com>
2024-03-04 13:58:46 +0800

    fix: runtime errors of the guide (#393)
    * perf: guide ui
    
    * fix: disable the 'swcMinify' configuration to fix runtime errors of the guide in the production
    
    * chore: remove redundant code

--- apps/nextjs-app/src/components/Guide.tsx
+++ apps/nextjs-app/src/components/Guide.tsx
@@ -71,7 +71,13 @@ export const Guide = ({ user }: { user?: IUserMeVo }) => {
         target: `.${GUIDE_CREATE_SPACE}`,
         title: <div className="text-base">{t('guide.createSpaceTooltipTitle')}</div>,
         content: (
-          <div className="text-left text-[13px]">{t('guide.createSpaceTooltipContent')}</div>
+          <div className="text-left text-[13px]">
+            <Trans
+              ns="common"
+              i18nKey="guide.createSpaceTooltipContent"
+              components={{ br: <br /> }}
+            />
+          </div>
         ),
         disableBeacon: true,
       },
@@ -103,7 +109,6 @@ export const Guide = ({ user }: { user?: IUserMeVo }) => {
           </div>
         ),
         disableBeacon: true,
-        placement: 'right',
       },
       [StepKey.ViewFiltering]: {
         target: `.${GUIDE_VIEW_FILTERING}`,
@@ -296,7 +301,7 @@ export const Guide = ({ user }: { user?: IUserMeVo }) => {
       disableScrollParentFix
       styles={{
         options: {
-          primaryColor: colors.violet[500],
+          primaryColor: colors.black,
           width: 320,
         },
         tooltip: {
