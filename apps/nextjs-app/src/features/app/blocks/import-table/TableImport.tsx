9f9acd27274b29f363516cb4ff3974e500e889ca
Mike <caoxing9@gmail.com>
2024-03-05 20:51:08 +0800

    feat: import support url (#403)
    * feat: import support url
    
    feat: import default field name add i18n
    
    * feat: supplement i18n in importcomponent

--- apps/nextjs-app/src/features/app/blocks/import-table/TableImport.tsx
+++ apps/nextjs-app/src/features/app/blocks/import-table/TableImport.tsx
@@ -30,11 +30,13 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  Input,
 } from '@teable/ui-lib';
 import { uniqBy } from 'lodash';
 import { useRouter } from 'next/router';
 import { useTranslation } from 'next-i18next';
 import { useState, useRef, useCallback } from 'react';
+import { z } from 'zod';
 import { CollapsePanel } from './CollapsePanel';
 import { FileItem } from './FileItem';
 import { PreviewColumn } from './PreviewColumn';
@@ -62,6 +64,7 @@ export const TableImport = (props: ITableImportProps) => {
   const [step, setStep] = useState(Step.UPLOAD);
   const { children, open, onOpenChange } = props;
   const [errorMessage, setErrorMessage] = useState('');
+  const [linkUrl, setLinkUrl] = useState('');
   const [alterDialogVisible, setAlterDialogVisible] = useState(false);
   const [files, setFiles] = useState<File[] | null>(null);
   const [fileInfo, setFileInfo] = useState<IAnalyzeRo>({} as IAnalyzeRo);
@@ -109,7 +112,7 @@ export const TableImport = (props: ITableImportProps) => {
       if (!options.useFirstRowAsHeader) {
         const newColumns = caculatedColumns?.map((item, index) => ({
           ...item,
-          name: `Field ${index + 1}`,
+          name: `${t('table:import.form.defaultFieldName')} ${index + 1}`,
         }));
         setCalculateColumns(newColumns);
       } else {
@@ -161,30 +164,42 @@ export const TableImport = (props: ITableImportProps) => {
     });
   };
 
-  const fileFinishedHandler = useCallback(async (result: INotifyVo) => {
-    const { presignedUrl } = result;
-    const analyzeData = await analyzeFile({
-      attachmentUrl: presignedUrl,
-      fileType: SUPPORTEDTYPE.CSV,
-    });
-    setFileInfo({
-      attachmentUrl: presignedUrl,
-      fileType: SUPPORTEDTYPE.CSV,
-    });
-    const {
-      data: { worksheets },
-    } = analyzeData;
+  const { mutateAsync: analyzeByUrl, isLoading: analyzeLoading } = useMutation({
+    mutationFn: analyzeFile,
+    onSuccess: (data, params) => {
+      const { attachmentUrl, fileType } = params;
+      setFileInfo({
+        attachmentUrl,
+        fileType,
+      });
+      const {
+        data: { worksheets },
+      } = data;
 
-    // TODO support groups
-    const calculatedColumnHeaders = worksheets?.[0].columns || [];
-    const columnHeaderWithIndex = calculatedColumnHeaders.map((col, index) => ({
-      ...col,
-      sourceColumnIndex: index,
-    }));
-    setCalculateColumns(columnHeaderWithIndex);
-    setStep(Step.CONFIG);
-    initCaculatedColumns.current = calculatedColumnHeaders;
-  }, []);
+      // TODO support groups
+      const calculatedColumnHeaders = worksheets?.[0].columns || [];
+      const columnHeaderWithIndex = calculatedColumnHeaders.map((col, index) => ({
+        ...col,
+        sourceColumnIndex: index,
+      }));
+      setCalculateColumns(columnHeaderWithIndex);
+      setStep(Step.CONFIG);
+      initCaculatedColumns.current = calculatedColumnHeaders;
+      setErrorMessage('');
+    },
+  });
+
+  const fileFinishedHandler = useCallback(
+    async (result: INotifyVo) => {
+      const { presignedUrl } = result;
+
+      await analyzeByUrl({
+        attachmentUrl: presignedUrl,
+        fileType: SUPPORTEDTYPE.CSV,
+      });
+    },
+    [analyzeByUrl]
+  );
 
   const dialogOpenProxy = (open: boolean) => {
     if (!open && Step.CONFIG && isLoading) {
@@ -201,9 +216,13 @@ export const TableImport = (props: ITableImportProps) => {
         {open && (
           <DialogContent className="flex max-h-[80%] w-[800px] min-w-[800px] max-w-fit flex-col overflow-hidden">
             <Tabs defaultValue="upload" className="flex-1 overflow-auto">
-              <TabsList className="">
-                <TabsTrigger value="upload">{t('table:import.title.import')}</TabsTrigger>
-              </TabsList>
+              {step === Step.UPLOAD && (
+                <TabsList>
+                  <TabsTrigger value="upload">{t('table:import.title.localFile')}</TabsTrigger>
+                  <TabsTrigger value="url">{t('table:import.title.linkUrl')}</TabsTrigger>
+                </TabsList>
+              )}
+
               <TabsContent value="upload">
                 {step === Step.UPLOAD && (
                   <div className="relative flex h-96 items-center justify-center">
@@ -252,23 +271,87 @@ export const TableImport = (props: ITableImportProps) => {
                   </div>
                 )}
               </TabsContent>
+              <TabsContent value="url">
+                {step === Step.UPLOAD && (
+                  <div className="flex h-32 w-full flex-col items-start px-2">
+                    <h4 className="m-2 text-sm">{t('table:import.title.linkUrlInputTitle')}</h4>
+                    <div className="flex w-full">
+                      <Input
+                        type="url"
+                        placeholder="https://www.example.com/file.png"
+                        className="mr-2 w-full"
+                        value={linkUrl}
+                        onChange={(e) => {
+                          const { value } = e.target;
+                          setLinkUrl(value);
+                        }}
+                      />
+                      <Button
+                        variant="outline"
+                        disabled={analyzeLoading || !linkUrl}
+                        onClick={() => {
+                          if (!linkUrl) {
+                            setErrorMessage(t('table:import.form.error.urlEmptyTip'));
+                            return;
+                          }
+                          if (!z.string().url().safeParse(linkUrl).success) {
+                            setErrorMessage(t('table:import.form.error.urlValidateTip'));
+                            return;
+                          }
+                          analyzeByUrl({
+                            attachmentUrl: linkUrl,
+                            fileType: SUPPORTEDTYPE.CSV,
+                          });
+                        }}
+                      >
+                        {analyzeLoading && <Spin className="mr-1 size-4" />}
+                        {t('table:import.title.upload')}
+                      </Button>
+                    </div>
+                    {errorMessage && <p className="p-2 text-sm text-red-500">{errorMessage}</p>}
+                  </div>
+                )}
+                {step === Step.CONFIG && (
+                  <div className="flex flex-col">
+                    <div>
+                      <p className="text-base font-bold">{t('table:import.title.importTitle')}</p>
+                    </div>
+
+                    <div className="my-2 h-[400px] overflow-y-auto rounded-sm border border-secondary">
+                      <PreviewColumn
+                        columns={caculatedColumns}
+                        onChange={columnsChangeHandler}
+                      ></PreviewColumn>
+                    </div>
+
+                    {errorMessage && <p className="pl-2 text-sm text-red-500">{errorMessage}</p>}
+
+                    <CollapsePanel
+                      onChange={optionChangeHandler}
+                      options={importOptions}
+                    ></CollapsePanel>
+                  </div>
+                )}
+              </TabsContent>
             </Tabs>
-            <DialogFooter>
-              <footer className="mt-1 flex items-center justify-end">
-                <Button size="sm" variant="secondary" onClick={() => closeDialog()}>
-                  {t('table:import.menu.cancel')}
-                </Button>
-                <Button
-                  size="sm"
-                  className="ml-1"
-                  onClick={() => importTable()}
-                  disabled={!!errorMessage || isLoading}
-                >
-                  {isLoading && <Spin className="mr-1 size-4" />}
-                  {t('table:import.title.import')}
-                </Button>
-              </footer>
-            </DialogFooter>
+            {step === Step.CONFIG && (
+              <DialogFooter>
+                <footer className="mt-1 flex items-center justify-end">
+                  <Button size="sm" variant="secondary" onClick={() => closeDialog()}>
+                    {t('table:import.menu.cancel')}
+                  </Button>
+                  <Button
+                    size="sm"
+                    className="ml-1"
+                    onClick={() => importTable()}
+                    disabled={!!errorMessage || isLoading}
+                  >
+                    {isLoading && <Spin className="mr-1 size-4" />}
+                    {t('table:import.title.import')}
+                  </Button>
+                </footer>
+              </DialogFooter>
+            )}
           </DialogContent>
         )}
       </Dialog>
