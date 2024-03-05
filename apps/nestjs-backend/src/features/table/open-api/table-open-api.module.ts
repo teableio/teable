9821f8db00abe00959a5922e3557c673b9a68335
tea artist <artist@teable.io>
2024-03-05 21:30:40 +0800

    fix: safe delete table with link field (#404)
    * fix: safe delete table with link field
    
    * chore: add tip for delete table

--- apps/nestjs-backend/src/features/table/open-api/table-open-api.module.ts
+++ apps/nestjs-backend/src/features/table/open-api/table-open-api.module.ts
@@ -3,6 +3,7 @@ import { DbProvider } from '../../../db-provider/db.provider';
 import { ShareDbModule } from '../../../share-db/share-db.module';
 import { CalculationModule } from '../../calculation/calculation.module';
 import { FieldCalculateModule } from '../../field/field-calculate/field-calculate.module';
+import { FieldOpenApiModule } from '../../field/open-api/field-open-api.module';
 import { GraphModule } from '../../graph/graph.module';
 import { RecordOpenApiModule } from '../../record/open-api/record-open-api.module';
 import { RecordModule } from '../../record/record.module';
@@ -17,6 +18,7 @@ import { TableOpenApiService } from './table-open-api.service';
     RecordModule,
     RecordOpenApiModule,
     ViewOpenApiModule,
+    FieldOpenApiModule,
     TableModule,
     ShareDbModule,
     CalculationModule,
