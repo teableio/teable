52c11ea84dc2612d2e931a19a4ee0dfb6e6b4080
xing_cx <caoxing9@gmail.com>
2024-03-05 15:18:02 +0800

    feat: update import ui

--- apps/nextjs-app/src/features/app/blocks/table-list/TableList.tsx
+++ apps/nextjs-app/src/features/app/blocks/table-list/TableList.tsx
@@ -45,7 +45,7 @@ export const TableList: React.FC = () => {
             </Button>
           </DropdownMenuItem>
           <DropdownMenuSeparator />
-          <DropdownMenuLabel className="px-4">
+          <DropdownMenuLabel className="px-4 text-xs font-normal text-muted-foreground">
             {t('table:import.menu.addFromOtherSource')}
           </DropdownMenuLabel>
           <DropdownMenuItem className="cursor-pointer" onClick={() => setDialogVisible(true)}>
