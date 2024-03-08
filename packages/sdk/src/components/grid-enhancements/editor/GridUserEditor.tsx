682b15a8ba7329223932136de3882107ad1e7779
Sky <sky.huang.fe@gmail.com>
2024-03-08 12:17:54 +0800

    fix: user cells cannot be searched using keyboard shortcuts

--- packages/sdk/src/components/grid-enhancements/editor/GridUserEditor.tsx
+++ packages/sdk/src/components/grid-enhancements/editor/GridUserEditor.tsx
@@ -1,15 +1,27 @@
 import type { IUserCellValue } from '@teable/core';
+import type { ForwardRefRenderFunction } from 'react';
+import { useRef, useImperativeHandle, forwardRef } from 'react';
 import type { UserField } from '../../../model';
+import type { IEditorRef } from '../../editor/type';
 import { UserEditorMain } from '../../editor/user';
 import type { IEditorProps } from '../../grid';
 import { useGridPopupPosition } from '../hooks';
 import type { IWrapperEditorProps } from './type';
 
-export const GridUserEditor = (props: IWrapperEditorProps & IEditorProps) => {
-  const { field, record, rect, style } = props;
+const GridUserEditorBase: ForwardRefRenderFunction<
+  IEditorRef<string>,
+  IWrapperEditorProps & IEditorProps
+> = (props, ref) => {
+  const { field, record, rect, style, isEditing } = props;
   const { id: fieldId, options } = field as UserField;
-
   const cellValue = record.getCellValue(field.id) as IUserCellValue | IUserCellValue[];
+
+  const defaultFocusRef = useRef<HTMLInputElement | null>(null);
+  const editorRef = useRef<IEditorRef<IUserCellValue | IUserCellValue[] | undefined>>(null);
+
+  useImperativeHandle(ref, () => ({
+    focus: () => (editorRef.current || defaultFocusRef.current)?.focus?.(),
+  }));
 
   const attachStyle = useGridPopupPosition(rect, 340);
   const onChange = (value?: IUserCellValue | IUserCellValue[]) => {
@@ -17,17 +29,26 @@ export const GridUserEditor = (props: IWrapperEditorProps & IEditorProps) => {
   };
 
   return (
-    <UserEditorMain
-      style={{
-        ...style,
-        ...attachStyle,
-        height: 'auto',
-        minWidth: 280,
-      }}
-      className="absolute rounded-sm border shadow-sm"
-      value={cellValue}
-      options={options}
-      onChange={onChange}
-    />
+    <>
+      {isEditing ? (
+        <UserEditorMain
+          ref={editorRef}
+          style={{
+            ...style,
+            ...attachStyle,
+            height: 'auto',
+            minWidth: 280,
+          }}
+          className="absolute rounded-sm border shadow-sm"
+          value={cellValue}
+          options={options}
+          onChange={onChange}
+        />
+      ) : (
+        <input className="size-0 opacity-0" ref={defaultFocusRef} />
+      )}
+    </>
   );
 };
+
+export const GridUserEditor = forwardRef(GridUserEditorBase);
