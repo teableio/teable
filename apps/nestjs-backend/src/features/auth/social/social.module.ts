50c3195c336cd3fbc2df1e4aca843fdfbab5377b
Boris <boris2code@outlook.com>
2024-03-19 10:45:31 +0800

    feat: support google oatuh2 (#466)

--- apps/nestjs-backend/src/features/auth/social/social.module.ts
+++ apps/nestjs-backend/src/features/auth/social/social.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConditionalModule } from '@nestjs/config';
 import { GithubModule } from './github/github.module';
+import { GoogleModule } from './google/google.module';
 
 @Module({
   imports: [
     ConditionalModule.registerWhen(GithubModule, (env) => {
       return Boolean(env.SOCIAL_AUTH_PROVIDERS?.split(',')?.includes('github'));
+    }),
+    ConditionalModule.registerWhen(GoogleModule, (env) => {
+      return Boolean(env.SOCIAL_AUTH_PROVIDERS?.split(',')?.includes('google'));
     }),
   ],
 })
