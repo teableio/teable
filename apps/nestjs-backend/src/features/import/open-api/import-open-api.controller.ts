b9601af673d0c5ee8107f041cccceb461b496eee
xing_cx <caoxing9@gmail.com>
2024-03-25 18:25:38 +0800

    feat: supplement import auth validate

--- apps/nestjs-backend/src/features/import/open-api/import-open-api.controller.ts
+++ apps/nestjs-backend/src/features/import/open-api/import-open-api.controller.ts
@@ -1,9 +1,9 @@
-/* eslint-disable sonarjs/no-duplicate-string */
 import { Controller, Get, UseGuards, Query, Post, Body, Param } from '@nestjs/common';
 import type { IAnalyzeVo } from '@teable/core';
 import { analyzeRoSchema, IAnalyzeRo, IImportOptionRo, importOptionRoSchema } from '@teable/core';
 import type { ITableFullVo } from '@teable/openapi';
 import { ZodValidationPipe } from '../../../zod.validation.pipe';
+import { Permissions } from '../../auth/decorators/permissions.decorator';
 import { PermissionGuard } from '../../auth/guard/permission.guard';
 import { ImportOpenApiService } from './import-open-api.service';
 
@@ -12,6 +12,7 @@ import { ImportOpenApiService } from './import-open-api.service';
 export class ImportController {
   constructor(private readonly importOpenService: ImportOpenApiService) {}
   @Get('/analyze')
+  @Permissions('table|import')
   async analyzeSheetFromFile(
     @Query(new ZodValidationPipe(analyzeRoSchema)) analyzeRo: IAnalyzeRo
   ): Promise<IAnalyzeVo> {
@@ -19,6 +20,7 @@ export class ImportController {
   }
 
   @Post(':baseId')
+  @Permissions('table|import')
   async createTableFromImport(
     @Param('baseId') baseId: string,
     @Body(new ZodValidationPipe(importOptionRoSchema)) importRo: IImportOptionRo
