4a72e41c2063e668c6921cfee48960f2516f7566
Sky <sky.huang.fe@gmail.com>
2024-04-07 22:24:18 +0800

    fix: disable the default right-click context menu in the browser

--- apps/nextjs-app/src/components/layout/MainLayout.tsx
+++ apps/nextjs-app/src/components/layout/MainLayout.tsx
@@ -3,7 +3,12 @@ import type { FC, ReactNode } from 'react';
 export const MainLayout: FC<{ children: ReactNode }> = (props) => {
   const { children } = props;
   return (
-    <div className="flex h-screen flex-col">
+    <div
+      className="flex h-screen flex-col"
+      onContextMenu={(e) => {
+        e.preventDefault();
+      }}
+    >
       <main>{children}</main>
     </div>
   );
