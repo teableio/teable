cb0f355c53aa6b17c45e6798cfa398df9d051d0a
xing_cx <caoxing9@gmail.com>
2024-04-09 16:57:36 +0800

    fix: inplace import field selector overflow can not scroll

--- apps/nextjs-app/src/features/app/blocks/import-table/field-config-panel/inplace-panel/FieldSelector.tsx
+++ apps/nextjs-app/src/features/app/blocks/import-table/field-config-panel/inplace-panel/FieldSelector.tsx
@@ -47,7 +47,7 @@ export function FieldSelector(props: IFieldSelector) {
   }
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
+    <Popover open={open} onOpenChange={setOpen} modal={true}>
       <PopoverTrigger className="w-full" asChild>
         <Button
           variant="outline"
