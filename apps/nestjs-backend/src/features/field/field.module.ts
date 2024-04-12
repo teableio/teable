6180627d8418e8aee751bf0a5d9fa6cdcb3cc141
boris <boris2code@outlook.com>
2024-04-12 15:27:36 +0800

    refactor: support for overriding field, view, and record permission services

--- apps/nestjs-backend/src/features/field/field.module.ts
+++ apps/nestjs-backend/src/features/field/field.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DbProvider } from '../../db-provider/db.provider';
 import { CalculationModule } from '../calculation/calculation.module';
-import { FieldPermissionService } from './field-permission.service';
 import { FieldService } from './field.service';
 
 @Module({
   imports: [CalculationModule],
-  providers: [FieldService, DbProvider, FieldPermissionService],
+  providers: [FieldService, DbProvider],
   exports: [FieldService],
 })
 export class FieldModule {}
--- apps/nestjs-backend/src/features/record/record.module.ts
+++ apps/nestjs-backend/src/features/record/record.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { DbProvider } from '../../db-provider/db.provider';
 import { AttachmentsStorageModule } from '../attachments/attachments-storage.module';
 import { CalculationModule } from '../calculation/calculation.module';
-import { RecordPermissionService } from './record-permission.service';
 import { RecordService } from './record.service';
 
 @Module({
   imports: [CalculationModule, AttachmentsStorageModule],
-  providers: [RecordService, DbProvider, RecordPermissionService],
+  providers: [RecordService, DbProvider],
   exports: [RecordService],
 })
 export class RecordModule {}
