9cfe841869f8dbdeeff6c3b9a1b514715b226780
Boris <boris2code@outlook.com>
2024-04-15 17:38:25 +0800

    fix: standardize the copy html structure to avoid too many meaningless dom node attributes (#570)

--- apps/nextjs-app/src/features/app/utils/clipboard.ts
+++ apps/nextjs-app/src/features/app/utils/clipboard.ts
@@ -1,5 +1,4 @@
 import { fieldVoSchema, parseClipboardText, type IFieldVo } from '@teable/core';
-import { mapValues } from 'lodash';
 import { fromZodError } from 'zod-validation-error';
 
 const teableHtmlMarker = 'data-teable-html-marker';
@@ -13,10 +12,7 @@ export const serializerHtml = (data: string, headers: IFieldVo[]) => {
     .join('');
   const headerContent = headers
     .map((header) => {
-      const attrs = Object.entries(header)
-        .map(([key, value]) => `${key}="${encodeURIComponent(JSON.stringify(value))}"`)
-        .join(' ');
-      return `<td ${attrs}>${header.name}</td>`;
+      return `<td id="${header.id}" data-field="${encodeURIComponent(JSON.stringify(header))}">${header.name}</td>`;
     })
     .join('');
 
@@ -36,38 +32,9 @@ export const extractTableHeader = (html?: string) => {
   const headers = Array.from(headerCells);
   let error = '';
   const result = headers.map((cell) => {
-    const id = cell.getAttribute('id');
-    const name = cell.getAttribute('name');
-    const isPrimary = cell.getAttribute('isPrimary');
-    const columnMeta = cell.getAttribute('columnMeta');
-    const dbFieldName = cell.getAttribute('dbFieldName');
-    const dbFieldType = cell.getAttribute('dbFieldType');
-    const type = cell.getAttribute('type');
-    const options = cell.getAttribute('options');
-    const cellValueType = cell.getAttribute('cellValueType');
-    const isComputed = cell.getAttribute('isComputed');
-    const isLookup = cell.getAttribute('isLookup');
-    const isMultipleCellValue = cell.getAttribute('isMultipleCellValue');
-    const fieldVo = mapValues(
-      {
-        id,
-        name,
-        isPrimary,
-        columnMeta,
-        dbFieldName,
-        dbFieldType,
-        type,
-        options,
-        cellValueType,
-        isComputed,
-        isLookup,
-        isMultipleCellValue,
-      },
-      (value) => {
-        const encodeValue = value ? decodeURIComponent(value) : undefined;
-        return encodeValue ? JSON.parse(encodeValue) : undefined;
-      }
-    );
+    const fieldVoStr = cell.getAttribute('data-field');
+    const fieldVo = fieldVoStr ? JSON.parse(decodeURIComponent(fieldVoStr)) : undefined;
+
     const validate = fieldVoSchema.safeParse(fieldVo);
     if (validate.success) {
       return fieldVo;
