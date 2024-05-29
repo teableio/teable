047146540310b28bf986e5d7bcd0db9fb0d949a9
Boris <boris2code@outlook.com>
2024-05-29 15:17:46 +0800

    fix: drag to the first (#631)

--- apps/nextjs-app/src/features/app/blocks/space/space-side-bar/PinList.tsx
+++ apps/nextjs-app/src/features/app/blocks/space/space-side-bar/PinList.tsx
@@ -81,7 +81,7 @@ export const PinList = () => {
 
     const [pin] = list.slice(from, from + 1);
 
-    const anchorPin = to === 0 ? list[1] : list[to - 1];
+    const anchorPin = to === 0 ? list[0] : list[to - 1];
     const position = to === 0 ? 'before' : 'after';
 
     updateOrder({
