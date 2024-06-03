a9a86ec6127554f592bd6eab5fcf7f2b9d2f05a2
Boris <boris2code@outlook.com>
2024-06-03 18:05:31 +0800

    fix: sort space cards based on pin order

--- apps/nextjs-app/src/features/app/blocks/space/useSpaceListOrdered.tsx
+++ apps/nextjs-app/src/features/app/blocks/space/useSpaceListOrdered.tsx
@@ -19,11 +19,14 @@ export const useSpaceListOrdered = () => {
     return [...spaceList.data].sort((a, b) => {
       const aPin = pinMap[a.id];
       const bPin = pinMap[b.id];
-      if (!bPin) {
-        return 1;
+      if (!aPin && !bPin) {
+        return 0; // Both a and b do not have a pin, maintain original order
       }
-      if (!aPin && bPin) {
-        return 1;
+      if (!aPin) {
+        return 1; // a does not have a pin, place a after b
+      }
+      if (!bPin) {
+        return -1; // b does not have a pin, place b after a
       }
       return aPin.order - bPin.order;
     });
