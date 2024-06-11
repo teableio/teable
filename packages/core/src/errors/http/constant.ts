992810a33b83dc1689d1749fa319df13f4312026
Boris <boris2code@outlook.com>
2024-06-11 15:10:42 +0800

    fix: share Unauthorized should be 401 (#659)

--- packages/core/src/errors/http/constant.ts
+++ packages/core/src/errors/http/constant.ts
@@ -4,8 +4,8 @@ import { HttpErrorCode } from './http-response.types';
 export const ErrorCodeToStatusMap: Record<HttpErrorCode, number> = {
   [HttpErrorCode.VALIDATION_ERROR]: 400,
   [HttpErrorCode.UNAUTHORIZED]: 401,
+  [HttpErrorCode.UNAUTHORIZED_SHARE]: 401,
   [HttpErrorCode.RESTRICTED_RESOURCE]: 403,
-  [HttpErrorCode.UNAUTHORIZED_SHARE]: 403,
   [HttpErrorCode.NOT_FOUND]: 404,
   [HttpErrorCode.INTERNAL_SERVER_ERROR]: 500,
   [HttpErrorCode.DATABASE_CONNECTION_UNAVAILABLE]: 503,
