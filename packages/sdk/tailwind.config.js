4748d3a658631df6d676f48cd275fd8c5df02866
Boris <boris2code@outlook.com>
2024-06-14 19:38:46 +0800

    fix: tailwindcss

--- packages/sdk/tailwind.config.js
+++ packages/sdk/tailwind.config.js
@@ -1,10 +1,11 @@
 const { join } = require('path');
 const uiConfig = require('@teable/ui-lib/ui.config.js');
 const sdkPath = join(__dirname, './src/**/*.{js,ts,jsx,tsx}');
+const buildFilePath = join(__dirname, './dist/**/*.{js,ts,jsx,tsx}');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = uiConfig({
-  content: [sdkPath],
+  content: [sdkPath, buildFilePath],
   darkMode: ['class'],
   theme: {},
   plugins: [],
---
+++ packages/sdk/ui.config.d.ts
@@ -0,0 +1,5 @@
+import type { Config } from 'tailwindcss';
+
+declare const configure: (config: Config) => Config;
+
+export default configure;
