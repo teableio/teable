b547ffe049c0a1a2350af235eae43767657107c5
Boris <boris2code@outlook.com>
2024-07-04 20:01:01 +0800

    fix: unit test

--- apps/nestjs-backend/src/features/oauth/oauth.controller.spec.ts
+++ apps/nestjs-backend/src/features/oauth/oauth.controller.spec.ts
@@ -1,12 +1,15 @@
 import type { TestingModule } from '@nestjs/testing';
 import { Test } from '@nestjs/testing';
+import { GlobalModule } from '../../global/global.module';
 import { OAuthController } from './oauth.controller';
+import { OAuthModule } from './oauth.module';
 
 describe('OauthController', () => {
   let controller: OAuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [GlobalModule, OAuthModule],
       controllers: [OAuthController],
     }).compile();
 
--- apps/nestjs-backend/src/features/oauth/oauth.service.spec.ts
+++ apps/nestjs-backend/src/features/oauth/oauth.service.spec.ts
@@ -1,5 +1,7 @@
 import type { TestingModule } from '@nestjs/testing';
 import { Test } from '@nestjs/testing';
+import { GlobalModule } from '../../global/global.module';
+import { OAuthModule } from './oauth.module';
 import { OAuthService } from './oauth.service';
 
 describe('OauthService', () => {
@@ -7,6 +9,7 @@ describe('OauthService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [GlobalModule, OAuthModule],
       providers: [OAuthService],
     }).compile();
 
