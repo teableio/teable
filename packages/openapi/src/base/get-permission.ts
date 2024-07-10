1caaa4df7a920224d74f94206eda749ebd480275
caoxing <caoxing9@gmail.com>
2024-07-10 19:51:58 +0800

    feat: add automation permission definition

--- packages/openapi/src/base/get-permission.ts
+++ packages/openapi/src/base/get-permission.ts
@@ -1,4 +1,4 @@
-import type { BaseActions, TableActions } from '@teable/core';
+import type { BaseActions, TableActions, AutomationActions } from '@teable/core';
 import { axios } from '../axios';
 import { registerRoute, urlBuilder } from '../utils';
 import { z } from '../zod';
@@ -6,7 +6,7 @@ import { z } from '../zod';
 export const GET_BASE_PERMISSION = '/base/{baseId}/permission';
 
 export const GetBasePermissionVoSchema = z.record(
-  z.custom<TableActions | BaseActions>(),
+  z.custom<TableActions | BaseActions | AutomationActions>(),
   z.boolean()
 );
 
