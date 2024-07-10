bb075563e02a5ad333237d11523e7ed0fa5b3f7c
Boris <boris2code@outlook.com>
2024-07-10 14:35:58 +0800

    feat: support OIDC (#617)
    * feat: support oidc
    
    * chore: console
    
    * feat: oidc rebase

--- apps/nestjs-backend/src/features/auth/social/github/github.controller.ts
+++ apps/nestjs-backend/src/features/auth/social/github/github.controller.ts
@@ -1,28 +1,24 @@
 import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { Response } from 'express';
-import type { IOauth2State } from '../../../../cache/types';
 import { Public } from '../../decorators/public.decorator';
 import { GithubGuard } from '../../guard/github.guard';
 import { SocialGuard } from '../../guard/social.guard';
+import { ControllerAdapter } from '../controller.adapter';
 
 @Controller('api/auth')
-export class GithubController {
+export class GithubController extends ControllerAdapter {
   @Get('/github')
   @Public()
   @UseGuards(GithubGuard)
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  async githubAuthenticate() {}
+  async githubAuthenticate() {
+    return super.authenticate();
+  }
 
   @Get('/github/callback')
   @Public()
   @UseGuards(SocialGuard, GithubGuard)
   async githubCallback(@Req() req: Express.Request, @Res({ passthrough: true }) res: Response) {
-    const user = req.user!;
-    // set cookie, passport login
-    await new Promise<void>((resolve, reject) => {
-      req.login(user, (err) => (err ? reject(err) : resolve()));
-    });
-    const redirectUri = (req.authInfo as { state: IOauth2State })?.state?.redirectUri;
-    return res.redirect(redirectUri || '/');
+    return super.callback(req, res);
   }
 }
--- apps/nestjs-backend/src/features/auth/social/social.module.ts
+++ apps/nestjs-backend/src/features/auth/social/social.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConditionalModule } from '@nestjs/config';
 import { GithubModule } from './github/github.module';
 import { GoogleModule } from './google/google.module';
+import { OIDCModule } from './oidc/oidc.module';
 
 @Module({
   imports: [
@@ -10,6 +11,9 @@ import { GoogleModule } from './google/google.module';
     }),
     ConditionalModule.registerWhen(GoogleModule, (env) => {
       return Boolean(env.SOCIAL_AUTH_PROVIDERS?.split(',')?.includes('google'));
+    }),
+    ConditionalModule.registerWhen(OIDCModule, (env) => {
+      return Boolean(env.SOCIAL_AUTH_PROVIDERS?.split(',')?.includes('oidc'));
     }),
   ],
 })
