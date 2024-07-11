9c7c793431967ded85413d0100d02a3bdb368c5c
Mike <caoxing9@gmail.com>
2024-07-11 12:08:03 +0800

    feat: add automation permission definition (#708)

--- apps/nestjs-backend/src/features/base/base.service.ts
+++ apps/nestjs-backend/src/features/base/base.service.ts
@@ -325,6 +325,7 @@ export class BaseService {
     return pick(permissionMap, [
       ...actionPrefixMap[ActionPrefix.Table],
       ...actionPrefixMap[ActionPrefix.Base],
+      ...actionPrefixMap[ActionPrefix.Automation],
     ]);
   }
 }
