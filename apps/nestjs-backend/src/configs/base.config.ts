93c5d446406dd537014f82a05486486eedb5171f
tea artist <artist@teable.io>
2024-08-07 18:43:48 +0800

    chore: better env example

--- apps/nestjs-backend/src/configs/base.config.ts
+++ apps/nestjs-backend/src/configs/base.config.ts
@@ -5,9 +5,9 @@ import { registerAs } from '@nestjs/config';
 
 export const baseConfig = registerAs('base', () => ({
   isCloud: process.env.NEXT_BUILD_ENV_EDITION?.toUpperCase() === 'CLOUD',
-  brandName: process.env.BRAND_NAME!,
-  publicOrigin: process.env.PUBLIC_ORIGIN!,
-  storagePrefix: process.env.STORAGE_PREFIX ?? process.env.PUBLIC_ORIGIN!,
+  brandName: process.env.BRAND_NAME,
+  publicOrigin: process.env.PUBLIC_ORIGIN,
+  storagePrefix: process.env.STORAGE_PREFIX ?? process.env.PUBLIC_ORIGIN,
   secretKey: process.env.SECRET_KEY ?? 'defaultSecretKey',
   publicDatabaseProxy: process.env.PUBLIC_DATABASE_PROXY,
   defaultMaxBaseDBConnections: Number(process.env.DEFAULT_MAX_BASE_DB_CONNECTIONS ?? 3),
