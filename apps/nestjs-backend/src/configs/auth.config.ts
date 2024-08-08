30930d196e0948ba4a97e921532564de4c30cc63
Bieber <artist@teable.io>
2024-08-08 15:14:11 +0700

    chore: better env example (#803)
    * chore: better env example
    
    * chore: incress db connection limit

--- apps/nestjs-backend/src/configs/auth.config.ts
+++ apps/nestjs-backend/src/configs/auth.config.ts
@@ -5,12 +5,14 @@ import { registerAs } from '@nestjs/config';
 
 export const authConfig = registerAs('auth', () => ({
   jwt: {
-    secret: process.env.BACKEND_JWT_SECRET ?? '533Cr3tK3yF0rH4sh1nGJ4W773k3n$',
+    secret:
+      process.env.BACKEND_JWT_SECRET ?? process.env.SECRET_KEY ?? '533Cr3tK3yF0rH4sh1nGJ4W773k3n$',
     expiresIn: process.env.BACKEND_JWT_EXPIRES_IN ?? '20d',
   },
   session: {
     secret:
       process.env.BACKEND_SESSION_SECRET ??
+      process.env.SECRET_KEY ??
       'dafea6be69af1c1c3b8caf2b609342f6eb4540b554e19539f7643b75b480c932',
     expiresIn: process.env.BACKEND_SESSION_EXPIRES_IN ?? '7d',
   },
