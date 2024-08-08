5f6a910663c126b422af4ae099078c2913152e75
tea artist <artist@teable.io>
2024-08-08 11:14:08 +0800

    chore: add date field value log

--- apps/nestjs-backend/src/features/field/model/field-dto/date-field.dto.ts
+++ apps/nestjs-backend/src/features/field/model/field-dto/date-field.dto.ts
@@ -20,9 +20,23 @@ export class DateFieldDto extends DateFieldCore implements FieldBase {
     if (value instanceof Date) {
       return value.toISOString();
     }
+
+    if (value instanceof Object && 'toISOString' in value) {
+      return (value as Date).toISOString();
+    }
+
+    console.warn('Unexpected data value: ', value);
+
+    if (value instanceof Object) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      console.warn('data value __proto__: ', (value as any).__proto__);
+      console.warn('data value constructor: ', value.constructor);
+    }
+
     if (typeof value === 'string' || typeof value === 'number') {
       return new Date(value).toISOString();
     }
+
     return value;
   }
 }
