6738df07e7e7e2cb9ae104a98e6269f181baba60
Boris <boris2code@outlook.com>
2024-08-13 17:56:25 +0800

    fix: computed field cell updates (#819)
    * fix: computed field cell updates
    
    * fix: useRecord on op batch
    
    * refactor: filter unchange field

--- packages/sdk/src/model/record/record.ts
+++ packages/sdk/src/model/record/record.ts
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import type { IRecord } from '@teable/core';
-import { RecordCore, FieldKeyType, RecordOpBuilder } from '@teable/core';
+import { RecordCore, FieldKeyType, RecordOpBuilder, FieldType } from '@teable/core';
 import type { ICreateRecordsRo, IGetRecordsRo, IUpdateRecordRo } from '@teable/openapi';
 import { createRecords, getRecords, updateRecord, updateRecordOrders } from '@teable/openapi';
+import { isEqual } from 'lodash';
 import type { Doc } from 'sharedb/lib/client';
 import { requestWrap } from '../../utils/requestWrap';
 import type { IFieldInstance } from '../field/factory';
@@ -38,12 +39,25 @@ export class Record extends RecordCore {
       oldCellValue,
     });
     this.doc.data.fields[fieldId] = cellValue;
-    this.doc.emit('op', [operation], false, '');
+    this.doc.emit('op batch', [operation], false);
     if (this.doc.version) {
       undo ? this.doc.version-- : this.doc.version++;
     }
     this.fields[fieldId] = cellValue;
   }
+
+  private updateComputedField = async (fieldIds: string[], record: IRecord) => {
+    const changeCellFieldIds = fieldIds.filter(
+      (fieldId) => !isEqual(this.fields[fieldId], record.fields[fieldId])
+    );
+    if (!changeCellFieldIds.length) {
+      return;
+    }
+    changeCellFieldIds.forEach((fieldId) => {
+      this.doc.data.fields[fieldId] = record.fields[fieldId];
+    });
+    this.doc.emit('op batch', [], false);
+  };
 
   async updateCell(fieldId: string, cellValue: unknown) {
     const oldCellValue = this.fields[fieldId];
@@ -51,7 +65,7 @@ export class Record extends RecordCore {
       this.onCommitLocal(fieldId, cellValue);
       this.fields[fieldId] = cellValue;
       const [, tableId] = this.doc.collection.split('_');
-      await Record.updateRecord(tableId, this.doc.id, {
+      const res = await Record.updateRecord(tableId, this.doc.id, {
         fieldKeyType: FieldKeyType.Id,
         record: {
           fields: {
@@ -59,6 +73,13 @@ export class Record extends RecordCore {
           },
         },
       });
+      const computedField = Object.keys(this.fieldMap).filter(
+        (fieldId) =>
+          this.fieldMap[fieldId].type === FieldType.Link || this.fieldMap[fieldId].isComputed
+      );
+      if (computedField.length) {
+        this.updateComputedField(computedField, res.data);
+      }
     } catch (error) {
       this.onCommitLocal(fieldId, oldCellValue, true);
       return error;
