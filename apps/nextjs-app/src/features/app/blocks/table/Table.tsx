37ff3d6c0e2e344ad266630f7e6e4fcab8f23f0b
tea artist <artist@teable.io>
2024-08-30 15:06:41 +0800

    fix: undo shortcut should work in form view

--- apps/nextjs-app/src/features/app/blocks/table/Table.tsx
+++ apps/nextjs-app/src/features/app/blocks/table/Table.tsx
@@ -1,10 +1,11 @@
 import type { IFieldVo, IRecord, IViewVo } from '@teable/core';
 import type { IGetBaseVo } from '@teable/openapi';
-import { AnchorContext, FieldProvider, useTable, ViewProvider } from '@teable/sdk';
+import { AnchorContext, FieldProvider, useTable, useUndoRedo, ViewProvider } from '@teable/sdk';
 import { TablePermissionProvider } from '@teable/sdk/context/table-permission';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { ErrorBoundary } from 'react-error-boundary';
+import { useHotkeys } from 'react-hotkeys-hook';
 import { View } from '../view/View';
 import { FailAlert } from './FailAlert';
 import { useViewErrorHandler } from './hooks/use-view-error-handler';
@@ -27,12 +28,21 @@ export const Table: React.FC<ITableProps> = ({
 }) => {
   const table = useTable();
   const router = useRouter();
+  const { undo, redo } = useUndoRedo();
   const { baseId, tableId, viewId } = router.query as {
     tableId: string;
     viewId: string;
     baseId: string;
   };
   useViewErrorHandler(baseId, tableId, viewId);
+  useHotkeys(`mod+z`, () => undo(), {
+    preventDefault: true,
+  });
+
+  useHotkeys([`mod+shift+z`, `mod+y`], () => redo(), {
+    preventDefault: true,
+  });
+
   return (
     <AnchorContext.Provider value={{ tableId, viewId, baseId }}>
       <Head>
