3deb41d17ab57db551b10ecf1504578956f967a1
tea artist <artist@teable.io>
2024-09-03 16:37:28 +0800

    fix: limit primary field type

--- apps/nextjs-app/src/features/app/components/field-setting/SelectFieldType.tsx
+++ apps/nextjs-app/src/features/app/components/field-setting/SelectFieldType.tsx
@@ -1,4 +1,4 @@
-import { FieldType } from '@teable/core';
+import { FieldType, PRIMARY_SUPPORTED_TYPES } from '@teable/core';
 import { FIELD_TYPE_ORDER, useFieldStaticGetter } from '@teable/sdk';
 import SearchIcon from '@teable/ui-lib/icons/app/search.svg';
 import { useTranslation } from 'next-i18next';
@@ -7,31 +7,37 @@ import { Selector } from '@/components/Selector';
 import { tableConfig } from '@/features/i18n/table.config';
 
 export const SelectFieldType = (props: {
+  isPrimary?: boolean;
   value?: FieldType | 'lookup';
   onChange?: (type: FieldType | 'lookup') => void;
 }) => {
-  const { value = FieldType.SingleLineText, onChange } = props;
+  const { isPrimary, value = FieldType.SingleLineText, onChange } = props;
   const getFieldStatic = useFieldStaticGetter();
   const { t } = useTranslation(tableConfig.i18nNamespaces);
 
-  const candidates = useMemo(
-    () =>
-      FIELD_TYPE_ORDER.map<{ id: FieldType | 'lookup'; name: string; icon: JSX.Element }>(
-        (type) => {
-          const { title, Icon } = getFieldStatic(type, false);
-          return {
-            id: type,
-            name: title,
-            icon: <Icon />,
-          };
-        }
-      ).concat({
-        id: 'lookup',
-        name: t('sdk:field.title.lookup'),
-        icon: <SearchIcon className="size-4" />,
-      }),
-    [getFieldStatic, t]
-  );
+  const candidates = useMemo(() => {
+    const fieldTypes = isPrimary
+      ? FIELD_TYPE_ORDER.filter((type) => PRIMARY_SUPPORTED_TYPES.has(type))
+      : FIELD_TYPE_ORDER;
+    const result = fieldTypes.map<{ id: FieldType | 'lookup'; name: string; icon: JSX.Element }>(
+      (type) => {
+        const { title, Icon } = getFieldStatic(type, false);
+        return {
+          id: type,
+          name: title,
+          icon: <Icon />,
+        };
+      }
+    );
+
+    return isPrimary
+      ? result
+      : result.concat({
+          id: 'lookup',
+          name: t('sdk:field.title.lookup'),
+          icon: <SearchIcon className="size-4" />,
+        });
+  }, [getFieldStatic, t, isPrimary]);
 
   return (
     <Selector
