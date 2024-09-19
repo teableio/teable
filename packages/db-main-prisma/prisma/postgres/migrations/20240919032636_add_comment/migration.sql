45ef062f1cfd85b144e0769323a0bd6426e48f6a
caoxing <caoxing9@gmail.com>
2024-09-19 11:28:00 +0800

    feat: add composite index for comment list table

--- packages/db-main-prisma/prisma/postgres/migrations/20240919032636_add_comment/migration.sql
+++ packages/db-main-prisma/prisma/postgres/migrations/20240919032636_add_comment/migration.sql
@@ -1,15 +1,17 @@
 -- CreateTable
 CREATE TABLE "comment" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "table_id" TEXT NOT NULL,
     "record_id" TEXT NOT NULL,
     "quote_Id" TEXT,
     "content" TEXT,
     "reaction" TEXT,
-    "deleted_time" DATETIME,
-    "created_time" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_time" TIMESTAMP(3),
+    "created_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "created_by" TEXT NOT NULL,
-    "last_modified_time" DATETIME
+    "last_modified_time" TIMESTAMP(3),
+
+    CONSTRAINT "comment_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -17,8 +19,11 @@ CREATE TABLE "comment_subscription" (
     "table_id" TEXT NOT NULL,
     "record_id" TEXT NOT NULL,
     "created_by" TEXT NOT NULL,
-    "created_time" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "created_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+-- CreateIndex
+CREATE INDEX "comment_table_id_record_id_idx" ON "comment"("table_id", "record_id");
 
 -- CreateIndex
 CREATE INDEX "comment_subscription_table_id_record_id_idx" ON "comment_subscription"("table_id", "record_id");
--- packages/db-main-prisma/prisma/sqlite/migrations/20240919032621_add_comment/migration.sql
+++ packages/db-main-prisma/prisma/sqlite/migrations/20240919032621_add_comment/migration.sql
@@ -1,17 +1,15 @@
 -- CreateTable
 CREATE TABLE "comment" (
-    "id" TEXT NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "table_id" TEXT NOT NULL,
     "record_id" TEXT NOT NULL,
     "quote_Id" TEXT,
     "content" TEXT,
     "reaction" TEXT,
-    "deleted_time" TIMESTAMP(3),
-    "created_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_time" DATETIME,
+    "created_time" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "created_by" TEXT NOT NULL,
-    "last_modified_time" TIMESTAMP(3),
-
-    CONSTRAINT "comment_pkey" PRIMARY KEY ("id")
+    "last_modified_time" DATETIME
 );
 
 -- CreateTable
@@ -19,8 +17,11 @@ CREATE TABLE "comment_subscription" (
     "table_id" TEXT NOT NULL,
     "record_id" TEXT NOT NULL,
     "created_by" TEXT NOT NULL,
-    "created_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "created_time" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+-- CreateIndex
+CREATE INDEX "comment_table_id_record_id_idx" ON "comment"("table_id", "record_id");
 
 -- CreateIndex
 CREATE INDEX "comment_subscription_table_id_record_id_idx" ON "comment_subscription"("table_id", "record_id");
