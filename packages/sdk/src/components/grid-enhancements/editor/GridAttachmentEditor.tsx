ef412f7cf0b4e4bc76093cfed62153678153d7fa
Boris <boris2code@outlook.com>
2024-10-09 16:27:13 +0800

    fix: focus style in attachment editor (#951)

--- packages/sdk/src/components/grid-enhancements/editor/GridAttachmentEditor.tsx
+++ packages/sdk/src/components/grid-enhancements/editor/GridAttachmentEditor.tsx
@@ -50,7 +50,7 @@ export const GridAttachmentEditor = forwardRef<
         <DialogContent
           container={containerRef.current}
           className={cn(
-            'click-outside-ignore flex-1 overflow-hidden max-w-xl',
+            'click-outside-ignore flex-1 overflow-hidden max-w-xl p-5 pt-8',
             Object.values(attachments || {}).length > 5 ? 'h-full max-h-[600px] mt-1 mb-1' : 'h-96'
           )}
         >
