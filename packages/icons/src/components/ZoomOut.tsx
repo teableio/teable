d1cd9343139f3d5595d1497c538b004a96a2137a
Boris <boris2code@outlook.com>
2024-10-17 14:47:00 +0800

    chore: update icons (#996)

---
+++ packages/icons/src/components/ZoomOut.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const ZoomOut = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M11 19a8 8 0 1 0 0-16 8 8 0 0 0 0 16M21 21l-4.35-4.35M8 11h6"
+    />
+  </svg>
+);
+export default ZoomOut;
