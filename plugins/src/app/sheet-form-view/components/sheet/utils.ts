88b578f5abf9f830cc8be2eacacfbc02d573ca27
caoxing <caoxing9@gmail.com>
2024-10-18 22:24:35 +0800

    perf: optimize sheet form value display

--- plugins/src/app/sheet-form-view/components/sheet/utils.ts
+++ plugins/src/app/sheet-form-view/components/sheet/utils.ts
@@ -1,7 +1,9 @@
 import type { IWorksheetData, ICellData } from '@univerjs/core';
 import { cloneDeep, has, isObject } from 'lodash';
 
+// to compatible with previous
 const exactCountPattern = /^\{\{([^:{}]+):([^{}]+)\}\}$/;
+const templateValueReg = /^\{\{[^}]*\}\}$/;
 
 export const clearTemplateMarker = (sheetData?: IWorksheetData['cellData']) => {
   if (!sheetData) {
@@ -14,7 +16,12 @@ export const clearTemplateMarker = (sheetData?: IWorksheetData['cellData']) => {
     if (has(newSheetData, key)) {
       for (const key2 in newSheetData[key]) {
         const cellValue = newSheetData[key][key2].v ?? '';
-        if (has(newSheetData[key], key2) && exactCountPattern.test(String(cellValue))) {
+        // new way to recognize template value
+        const customer = newSheetData[key][key2].custom;
+        if (
+          (has(newSheetData[key], key2) && exactCountPattern.test(String(cellValue))) ||
+          (isObject(customer) && has(customer, 'fieldId'))
+        ) {
           newSheetData[key][key2].v = undefined;
         }
       }
@@ -24,6 +31,40 @@ export const clearTemplateMarker = (sheetData?: IWorksheetData['cellData']) => {
   return newSheetData;
 };
 
+export const clearChangedTemplateValue = (sheetData?: IWorksheetData['cellData']) => {
+  if (!sheetData) {
+    return {
+      cellData: sheetData,
+      deletedFields: [] as string[],
+    };
+  }
+
+  const newSheetData = cloneDeep(sheetData);
+  const deletedFields = [];
+
+  for (const key in newSheetData) {
+    if (has(newSheetData, key)) {
+      for (const key2 in newSheetData[key]) {
+        const cellValue = newSheetData[key][key2].v ?? '';
+        if (
+          (has(newSheetData[key], key2) &&
+            !templateValueReg.test(String(cellValue)) &&
+            Boolean(newSheetData[key][key2]?.custom?.fieldId)) ||
+          !exactCountPattern.test(String(cellValue))
+        ) {
+          deletedFields.push(newSheetData[key][key2]?.custom?.fieldId as string);
+          delete newSheetData[key][key2].custom;
+        }
+      }
+    }
+  }
+
+  return {
+    cellData: newSheetData,
+    deletedFields: deletedFields as string[],
+  };
+};
+
 export const getRecordRangesMap = (sheetData?: IWorksheetData['cellData']) => {
   const rangesMap: Record<string, [number, number]> = {};
 
@@ -31,19 +72,27 @@ export const getRecordRangesMap = (sheetData?: IWorksheetData['cellData']) => {
     return rangesMap;
   }
 
-  for (const [key, row] of Object.entries(sheetData)) {
-    if (!isObject(row)) {
-      continue;
+  const processCell = (key: string, key2: string, cell: ICellData) => {
+    const { v: cellValue } = cell;
+    const match = typeof cellValue === 'string' ? cellValue.match(exactCountPattern) : null;
+
+    if (match) {
+      const fieldId = match[2];
+      rangesMap[fieldId] = [parseInt(key), parseInt(key2)];
     }
-    for (const [key2, cell] of Object.entries(row)) {
-      if (!isObject(cell)) {
-        continue;
-      }
-      const { v: cellValue } = cell as ICellData;
-      const match = typeof cellValue === 'string' ? cellValue?.match(exactCountPattern) : null;
-      if (match) {
-        const fieldId = match[2];
-        rangesMap[fieldId] = [parseInt(key), parseInt(key2)];
+
+    if (cell.custom?.fieldId) {
+      const fieldId = cell.custom.fieldId;
+      rangesMap[fieldId] = [parseInt(key), parseInt(key2)];
+    }
+  };
+
+  for (const [key, row] of Object.entries(sheetData)) {
+    if (isObject(row)) {
+      for (const [key2, cell] of Object.entries(row)) {
+        if (isObject(cell)) {
+          processCell(key, key2, cell);
+        }
       }
     }
   }
