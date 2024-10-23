60f4c31c4d93e850dc15b87b8a6399917e16fb5d
Mike <caoxing9@gmail.com>
2024-10-23 15:25:05 +0800

    fix: compositing filter input should not be overwrited (#1017)
    * fix: compositing filter input should not be overwrited
    
    * perf: keep sync from socket when input is not focus

--- packages/sdk/src/components/filter/view-filter/component/FilterInput.tsx
+++ packages/sdk/src/components/filter/view-filter/component/FilterInput.tsx
@@ -1,6 +1,7 @@
 import type { IFilterItem } from '@teable/core';
 import { Input, cn } from '@teable/ui-lib';
 import { toString } from 'lodash';
+import { useEffect, useRef, useState } from 'react';
 import { useTranslation } from '../../../../context/app/i18n';
 
 interface InputProps {
@@ -14,13 +15,39 @@ const FilterInput = (props: InputProps) => {
   const { t } = useTranslation();
   const { onChange, placeholder = t('filter.default.placeholder'), value, className } = props;
   const inputValue = toString(value);
+  const [compositing, setCompositing] = useState(false);
+  const [stashValue, setStashValue] = useState(inputValue);
+  const [isFocus, setIsFocus] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current && !isFocus) {
+      inputRef.current.value = inputValue;
+    }
+  }, [inputValue, isFocus]);
 
   return (
     <Input
       placeholder={placeholder}
-      value={inputValue}
-      onChange={(e) => {
-        onChange(e.target.value || null);
+      defaultValue={inputValue}
+      ref={inputRef}
+      onInput={(e) => {
+        setStashValue(e.currentTarget.value);
+        !compositing && onChange(e.currentTarget.value || null);
+      }}
+      onCompositionStart={() => setCompositing(true)}
+      onCompositionEnd={() => {
+        setCompositing(false);
+        onChange(stashValue || null);
+      }}
+      onBlur={() => {
+        setIsFocus(false);
+      }}
+      onFocus={() => {
+        if (!isFocus) {
+          setIsFocus(true);
+        }
       }}
       className={cn('h-8 placeholder:text-xs', className)}
     />
