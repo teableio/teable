3b74837e18417bee166cfbec5f60fc0f99a2f5ee
caoxing <caoxing9@gmail.com>
2024-10-25 17:26:57 +0800

    fix: loop to call `useAttachmentPreviewI18Map` function

--- packages/sdk/src/components/hooks/useAttachmentPreviewI18Map.ts
+++ packages/sdk/src/components/hooks/useAttachmentPreviewI18Map.ts
@@ -1,9 +1,13 @@
+import { useMemo } from 'react';
 import { useTranslation } from '../../context/app/i18n';
 
 export const useAttachmentPreviewI18Map = () => {
   const { t } = useTranslation();
-  return {
-    previewFileLimit: t('preview.previewFileLimit', { size: 10 }),
-    loadFileError: t('preview.loadFileError'),
-  };
+  return useMemo(
+    () => ({
+      previewFileLimit: t('preview.previewFileLimit', { size: 10 }),
+      loadFileError: t('preview.loadFileError'),
+    }),
+    [t]
+  );
 };
