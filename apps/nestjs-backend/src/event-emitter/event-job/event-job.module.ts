ec62c440ab2666e32a8d009495b99b76aa07dbfc
Boris <boris2code@outlook.com>
2024-11-01 12:15:49 +0800

    chore: update queue config

--- apps/nestjs-backend/src/event-emitter/event-job/event-job.module.ts
+++ apps/nestjs-backend/src/event-emitter/event-job/event-job.module.ts
@@ -1,9 +1,19 @@
 import { BullModule } from '@nestjs/bullmq';
+import type { NestWorkerOptions } from '@nestjs/bullmq/dist/interfaces/worker-options.interface';
 import type { DynamicModule } from '@nestjs/common';
 import { Module } from '@nestjs/common';
 import { ConditionalModule } from '@nestjs/config';
 import { ConfigModule } from '../../configs/config.module';
 import { FallbackQueueModule } from './fallback/fallback-queue.module';
+
+const queueOptions: NestWorkerOptions = {
+  removeOnComplete: {
+    count: 2000,
+  },
+  removeOnFail: {
+    count: 5000,
+  },
+};
 
 @Module({
   imports: [ConfigModule],
@@ -11,8 +21,12 @@ import { FallbackQueueModule } from './fallback/fallback-queue.module';
 export class EventJobModule {
   static async registerQueue(name: string): Promise<DynamicModule> {
     const [bullQueue, fallbackQueue] = await Promise.all([
-      ConditionalModule.registerWhen(BullModule.registerQueue({ name }), (env) =>
-        Boolean(env.BACKEND_CACHE_REDIS_URI)
+      ConditionalModule.registerWhen(
+        BullModule.registerQueue({
+          name,
+          ...queueOptions,
+        }),
+        (env) => Boolean(env.BACKEND_CACHE_REDIS_URI)
       ),
       ConditionalModule.registerWhen(
         FallbackQueueModule.registerQueue(name),
