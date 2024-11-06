a0af1ef47ee5e098536adef27330ecadd60c1f4e
Boris <boris2code@outlook.com>
2024-11-06 17:55:15 +0800

    feat: remove table headers from copyed html

--- apps/nextjs-app/src/features/app/utils/clipboard.ts
+++ apps/nextjs-app/src/features/app/utils/clipboard.ts
@@ -1,7 +1,9 @@
 import { FieldType, fieldVoSchema, parseClipboardText, type IFieldVo } from '@teable/core';
+import { z } from 'zod';
 import { fromZodError } from 'zod-validation-error';
 
 const teableHtmlMarker = 'data-teable-html-marker';
+const teableHeader = 'data-teable-html-header';
 
 export const serializerHtml = (data: string, headers: IFieldVo[]) => {
   const tableData = parseClipboardText(data);
@@ -18,13 +20,8 @@ export const serializerHtml = (data: string, headers: IFieldVo[]) => {
         .join('')}</tr>`;
     })
     .join('');
-  const headerContent = headers
-    .map((header) => {
-      return `<th id="${header.id}" data-field="${encodeURIComponent(JSON.stringify(header))}">${header.name}</th>`;
-    })
-    .join('');
 
-  return `<meta charset="utf-8"><table ${teableHtmlMarker}="1"><thead><tr>${headerContent}</tr></thead><tbody>${bodyContent}</tbody></table>`;
+  return `<meta charset="utf-8"><table ${teableHtmlMarker}="1" ${teableHeader}="${encodeURIComponent(JSON.stringify(headers))}"><tbody>${bodyContent}</tbody></table>`;
 };
 
 export const extractTableHeader = (html?: string) => {
@@ -34,23 +31,16 @@ export const extractTableHeader = (html?: string) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(html, 'text/html');
   const table = doc.querySelector('table');
-  const headerRow = table?.querySelector('thead tr');
-  const headerCells = headerRow?.querySelectorAll('th') || [];
-
-  const headers = Array.from(headerCells);
-  let error = '';
-  const result = headers.map((cell) => {
-    const fieldVoStr = cell.getAttribute('data-field');
-    const fieldVo = fieldVoStr ? JSON.parse(decodeURIComponent(fieldVoStr)) : undefined;
-
-    const validate = fieldVoSchema.safeParse(fieldVo);
-    if (validate.success) {
-      return fieldVo;
-    }
-    error = fromZodError(validate.error).message;
-    return undefined;
-  }) as IFieldVo[];
-  return error ? { result: undefined, error } : { result };
+  const headerStr = table?.getAttribute(teableHeader);
+  const headers = headerStr ? JSON.parse(decodeURIComponent(headerStr)) : undefined;
+  if (!headers) {
+    return { result: undefined };
+  }
+  const validate = z.array(fieldVoSchema).safeParse(headers);
+  if (!validate.success) {
+    return { result: undefined, error: fromZodError(validate.error).message };
+  }
+  return { result: validate.data };
 };
 
 export const isTeableHTML = (html: string) => {
