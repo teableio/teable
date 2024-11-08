b274e7f1d1647aa4e5a26be59a58249d59a298fe
caoxing <caoxing9@gmail.com>
2024-11-08 21:58:21 +0800

    test: add e2e unit

--- apps/nestjs-backend/test/aggregation-search.e2e-spec.ts
+++ apps/nestjs-backend/test/aggregation-search.e2e-spec.ts
@@ -1,6 +1,6 @@
 import type { INestApplication } from '@nestjs/common';
 import type { ITableFullVo } from '@teable/openapi';
-import { getSearchCount, getSearchIndex, createField } from '@teable/openapi';
+import { getSearchCount, getSearchIndex, createField, updateViewColumnMeta } from '@teable/openapi';
 import { x_20 } from './data-helpers/20x';
 import { x_20_link, x_20_link_from_lookups } from './data-helpers/20x-link';
 
@@ -9,13 +9,13 @@ import { createTable, permanentDeleteTable, initApp } from './utils/init-app';
 describe('OpenAPI AggregationController (e2e)', () => {
   let app: INestApplication;
   let table: ITableFullVo;
+  let subTable: ITableFullVo;
   const baseId = globalThis.testConfig.baseId;
 
   afterAll(async () => {
     await app.close();
   });
 
-  let subTable: ITableFullVo;
   beforeAll(async () => {
     const appCtx = await initApp();
     app = appCtx.app;
@@ -51,6 +51,24 @@ describe('OpenAPI AggregationController (e2e)', () => {
       expect(result?.data?.count).toBe(22);
     });
 
+    it('should filter the hidden filed', async () => {
+      const result = await getSearchCount(table.id, {
+        search: ['1', '', false],
+      });
+      await updateViewColumnMeta(table.id, table.views[0].id, [
+        {
+          fieldId: table.fields[1].id,
+          columnMeta: { hidden: true },
+        },
+      ]);
+      const result2 = await getSearchCount(table.id, {
+        search: ['1', '', false],
+        viewId: table.views[0].id,
+      });
+      expect(result?.data?.count).toBe(87);
+      expect(result2?.data?.count).toBe(75);
+    });
+
     it('should return 0 when there is no result', async () => {
       const result = await getSearchCount(table.id, {
         search: ['Go to Gentle night', '', false],
@@ -69,7 +87,9 @@ describe('OpenAPI AggregationController (e2e)', () => {
       expect(result2?.data?.fieldId).toBe(table.fields[0].id);
     });
 
-    it.only('should return null when there is no found', async () => {
+    it('should');
+
+    it('should return null when there is no found', async () => {
       const result2 = await getSearchIndex(table.id, {
         index: 1,
         search: ['Go to Gentle night', '', false],
