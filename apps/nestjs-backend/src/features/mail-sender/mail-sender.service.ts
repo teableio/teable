27fab90728c8d4b6f2162cc26272f7043b6b1df3
Boris <boris2code@outlook.com>
2024-11-11 15:26:45 +0800

    feat: add email verify code templates

--- apps/nestjs-backend/src/features/mail-sender/mail-sender.service.ts
+++ apps/nestjs-backend/src/features/mail-sender/mail-sender.service.ts
@@ -26,6 +26,7 @@ export class MailSenderService {
 
     return sender.catch((reason) => {
       if (reason) {
+        console.error(reason);
         this.logger.error(`Mail sending failed: ${reason.message}`, reason.stack);
       }
       return false;
@@ -131,6 +132,18 @@ export class MailSenderService {
         email,
         resetPasswordUrl,
         partialBody: 'reset-password',
+      },
+    };
+  }
+
+  sendEmailVerifyCodeEmailOptions(info: { title: string; message: string }) {
+    const { title } = info;
+    return {
+      subject: `${title} - ${this.baseConfig.brandName}`,
+      template: 'normal',
+      context: {
+        partialBody: 'email-verify-code',
+        ...info,
       },
     };
   }
