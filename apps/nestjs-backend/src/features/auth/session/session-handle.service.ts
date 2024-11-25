ecc7e54600f747c38775eb255b56557f0c69d756
Boris <boris2code@outlook.com>
2024-11-25 19:37:23 +0800

    chore: log error when user cookie is missing

--- apps/nestjs-backend/src/features/auth/session/session-handle.service.ts
+++ apps/nestjs-backend/src/features/auth/session/session-handle.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import type { Request, RequestHandler } from 'express';
 import session from 'express-session';
 import ms from 'ms';
@@ -8,6 +8,7 @@ import { SessionStoreService } from './session-store.service';
 
 @Injectable()
 export class SessionHandleService {
+  private readonly logger = new Logger(SessionHandleService.name);
   sessionMiddleware: RequestHandler;
   constructor(
     private readonly sessionStoreService: SessionStoreService,
@@ -31,6 +32,9 @@ export class SessionHandleService {
       this.sessionMiddleware(request, {} as any, (err) => {
         if (err) {
           return reject(err);
+        }
+        if (!request.cookies[AUTH_SESSION_COOKIE_NAME]) {
+          this.logger.error('Auth session cookie is not found in request cookies');
         }
         resolve(request.sessionID);
       });
