1e9b265cbcf9c9b656656ffc98f7622ccbbca14e
tea artist <artist@teable.io>
2024-11-27 21:13:07 +0800

    chore: publish 1.5.2 release

--- apps/nextjs-app/src/pages/auth/signup.tsx
+++ apps/nextjs-app/src/pages/auth/signup.tsx
@@ -1,34 +1,5 @@
-import { QueryClientProvider } from '@tanstack/react-query';
-import { createQueryClient } from '@teable/sdk/context';
-import type { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import { useState } from 'react';
-import { LoginPage } from '@/features/auth/pages/LoginPage';
-import { authConfig } from '@/features/i18n/auth.config';
-import ensureLogin from '@/lib/ensureLogin';
-import { getTranslationsProps } from '@/lib/i18n';
-import withEnv from '@/lib/withEnv';
+import LoginRoute from './login';
 
-type Props = {
-  /** Add props here */
-};
+export { getServerSideProps } from './login';
 
-export default function LoginRoute(_props: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const [queryClient] = useState(() => createQueryClient());
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <LoginPage />
-    </QueryClientProvider>
-  );
-}
-
-export const getServerSideProps: GetServerSideProps<Props> = withEnv(
-  ensureLogin(async (context) => {
-    const { i18nNamespaces } = authConfig;
-    return {
-      props: {
-        ...(await getTranslationsProps(context, i18nNamespaces)),
-      },
-    };
-  }, true)
-);
+export default LoginRoute;
