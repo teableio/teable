cee4e3241901e6966181623659673f99aa257b3f
SkyHuang <sky.huang.fe@gmail.com>
2024-11-27 14:41:43 +0800

    perf: add loading animation when loading data in calendar view

--- apps/nextjs-app/src/features/app/blocks/view/calendar/components/Calendar.tsx
+++ apps/nextjs-app/src/features/app/blocks/view/calendar/components/Calendar.tsx
@@ -10,7 +10,7 @@ import type { EventResizeDoneArg } from '@fullcalendar/interaction';
 import interactionPlugin from '@fullcalendar/interaction';
 import FullCalendar from '@fullcalendar/react';
 import { FieldKeyType } from '@teable/core';
-import { ChevronLeft, ChevronRight, Calendar as CalendarIcon } from '@teable/icons';
+import { ChevronLeft, ChevronRight, Calendar as CalendarIcon, Loader2 } from '@teable/icons';
 import { updateRecord } from '@teable/openapi';
 import { AppContext, CalendarDailyCollectionContext } from '@teable/sdk/context';
 import { useTableId } from '@teable/sdk/hooks';
@@ -25,6 +25,7 @@ import {
   PopoverContent,
   PopoverTrigger,
   Calendar as DatePicker,
+  cn,
 } from '@teable/ui-lib/shadcn';
 import { addDays, format } from 'date-fns';
 import { enUS, zhCN, ja, ru, fr } from 'date-fns/locale';
@@ -203,6 +204,7 @@ export const Calendar = (props: ICalendarProps) => {
     });
   };
 
+  const isLoading = !calendarDailyCollection;
   const { countMap, records = [] } = calendarDailyCollection ?? {};
 
   const events = useMemo(() => {
@@ -372,8 +374,14 @@ export const Calendar = (props: ICalendarProps) => {
     <div className="relative flex size-full flex-col overflow-hidden p-4 pt-2" ref={containerRef}>
       <div className="mb-2 flex items-center justify-between">
         <div className="flex items-center gap-2">
-          <h2 className="text-xl font-semibold">
+          <h2 className="flex items-center text-xl font-semibold">
             {title || calendarRef.current?.getApi().view.title}
+            <Loader2
+              className={cn(
+                'ml-1 size-5 animate-spin transition-opacity duration-1000',
+                isLoading ? 'opacity-100' : 'opacity-0'
+              )}
+            />
           </h2>
         </div>
         <div className="flex items-center gap-2">
