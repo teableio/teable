0bc8cb02694af3c68c23c6a4b6d8859b97747b26
SkyHuang <sky.huang.fe@gmail.com>
2024-11-28 12:14:02 +0800

    fix: incorrect filter conditions when viewing more events in the calendar

--- apps/nextjs-app/src/features/app/blocks/view/calendar/components/EventListContainer.tsx
+++ apps/nextjs-app/src/features/app/blocks/view/calendar/components/EventListContainer.tsx
@@ -1,6 +1,7 @@
 import type { IFilter } from '@teable/core';
-import { mergeFilter, and, exactDate, isOnOrBefore, isOnOrAfter } from '@teable/core';
+import { mergeFilter, and, exactDate, isOnOrBefore, isOnOrAfter, or, is } from '@teable/core';
 import { RowCountProvider } from '@teable/sdk/context';
+import { endOfDay, startOfDay } from 'date-fns';
 import { useMemo } from 'react';
 import { useCalendar } from '../hooks';
 import { EventList } from './EventList';
@@ -16,28 +17,45 @@ export const EventListContainer = (props: IEventListContainerProps) => {
   const query = useMemo(() => {
     if (!startDateField || !endDateField) return;
 
-    const dateStr = date.toISOString();
-
     const filter = mergeFilter(recordQuery?.filter, {
       conjunction: and.value,
       filterSet: [
         {
-          fieldId: startDateField.id,
-          operator: isOnOrBefore.value,
-          value: {
-            exactDate: dateStr,
-            mode: exactDate.value,
-            timeZone: startDateField.options.formatting.timeZone,
-          },
-        },
-        {
-          fieldId: endDateField.id,
-          operator: isOnOrAfter.value,
-          value: {
-            exactDate: dateStr,
-            mode: exactDate.value,
-            timeZone: endDateField.options.formatting.timeZone,
-          },
+          conjunction: or.value,
+          filterSet: [
+            {
+              conjunction: and.value,
+              filterSet: [
+                {
+                  fieldId: startDateField.id,
+                  operator: isOnOrBefore.value,
+                  value: {
+                    exactDate: endOfDay(date).toISOString(),
+                    mode: exactDate.value,
+                    timeZone: startDateField.options.formatting.timeZone,
+                  },
+                },
+                {
+                  fieldId: endDateField.id,
+                  operator: isOnOrAfter.value,
+                  value: {
+                    exactDate: startOfDay(date).toISOString(),
+                    mode: exactDate.value,
+                    timeZone: endDateField.options.formatting.timeZone,
+                  },
+                },
+              ],
+            },
+            {
+              fieldId: startDateField.id,
+              operator: is.value,
+              value: {
+                exactDate: date.toISOString(),
+                mode: exactDate.value,
+                timeZone: startDateField.options.formatting.timeZone,
+              },
+            },
+          ],
         },
       ],
     }) as IFilter;
