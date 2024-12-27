7b5aa2fdcf55bb921b5c34ced8369d89f6fa4bf3
SkyHuang <sky.huang.fe@gmail.com>
2024-12-27 18:21:07 +0800

    chore: update icons

---
+++ packages/icons/src/components/Mistral.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const Mistral = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path fill="#FECE00" d="M3.6 3h3.6v3.6H3.6zM18 3h3.6v3.6H18z" />
+    <path fill="#FFA301" d="M3.6 6.6h3.6v3.6H3.6z" />
+    <path fill="#FE4900" d="M3.6 13.8h3.6v3.6H3.6zM10.8 13.8h3.6v3.6h-3.6zM18 13.8h3.6v3.6H18z" />
+    <path fill="#FFA301" d="M14.4 6.6H18v3.6h-3.6z" />
+    <path fill="#FF6F00" d="M3.6 10.2h3.6v3.6H3.6z" />
+    <path fill="#FE0107" d="M3.6 17.4h3.6V21H3.6zM18 17.4h3.6V21H18z" />
+    <path fill="#FF6F00" d="M14.4 10.2H18v3.6h-3.6z" />
+    <path fill="#FFA301" d="M7.2 6.6h3.6v3.6H7.2zM18 6.6h3.6v3.6H18z" />
+    <path fill="#FF6F00" d="M7.2 10.2h3.6v3.6H7.2zM18 10.2h3.6v3.6H18zM10.8 10.2h3.6v3.6h-3.6z" />
+    <path
+      fill="#191D1D"
+      d="M2.4 3h1.2v18H2.4zM16.8 3H18v3.6h-1.2zM13.2 6.6h1.2v3.6h-1.2zM9.6 13.8h1.2v3.6H9.6zM16.8 13.8H18V21h-1.2z"
+    />
+  </svg>
+);
+export default Mistral;
