3fc981f7194508266fad5d3d8e2c7a5c96456d2d
SkyHuang <sky.huang.fe@gmail.com>
2025-02-19 14:14:11 +0800

    fix: lint type

--- packages/sdk/src/context/table-permission/TablePermissionProvider.tsx
+++ packages/sdk/src/context/table-permission/TablePermissionProvider.tsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import type { ITableActionKey } from '@teable/core';
 import { getTablePermission } from '@teable/openapi';
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import { ReactQueryKeys } from '../../config';
 import { useTableId, useTableListener } from '../../hooks';
 import {
@@ -23,8 +23,12 @@ export const TablePermissionProvider = ({
     enabled: !!tableId,
   });
 
+  const refetchTablePermission = useCallback(() => {
+    refetch();
+  }, [refetch]);
+
   const tableMatches = useMemo<ITableActionKey[]>(() => ['addField'], []);
-  useTableListener(tableId, tableMatches, refetch);
+  useTableListener(tableId, tableMatches, refetchTablePermission);
 
   return (
     <TablePermissionContext.Provider value={tablePermission ?? TablePermissionContextDefaultValue}>
