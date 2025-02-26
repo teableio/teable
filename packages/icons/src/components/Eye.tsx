b6507e9142501e4bda36cf5391d3b5da25d2f8b4
SkyHuang <sky.huang.fe@gmail.com>
2025-02-26 16:22:05 +0800

    chore: update icons

---
+++ packages/icons/src/components/Eye.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const Eye = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7"
+    />
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6"
+    />
+  </svg>
+);
+export default Eye;
