9e699de68ee50dad61da73f8088ed2ba1d0d7cb9
SkyHuang <sky.huang.fe@gmail.com>
2025-04-09 17:35:59 +0800

    chore: db migration

--- packages/db-main-prisma/prisma/sqlite/migrations/20250409093334_add_task_tables/migration.sql
+++ packages/db-main-prisma/prisma/sqlite/migrations/20250409093334_add_task_tables/migration.sql
@@ -1,40 +1,39 @@
 -- AlterTable
-ALTER TABLE "field" ADD COLUMN     "ai_config" TEXT;
+ALTER TABLE "field" ADD COLUMN "ai_config" TEXT;
 
 -- CreateTable
 CREATE TABLE "task" (
-    "id" TEXT NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "type" TEXT NOT NULL,
     "status" TEXT NOT NULL,
     "snapshot" TEXT,
-    "created_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "task_pkey" PRIMARY KEY ("id")
+    "created_time" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 -- CreateTable
 CREATE TABLE "task_run" (
-    "id" TEXT NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "task_id" TEXT NOT NULL,
     "status" TEXT NOT NULL,
     "snapshot" TEXT NOT NULL,
     "spent" INTEGER,
-    "started_time" TIMESTAMP(3),
-    "created_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "last_modified_time" TIMESTAMP(3),
-
-    CONSTRAINT "task_run_pkey" PRIMARY KEY ("id")
+    "error_msg" TEXT,
+    "started_time" DATETIME,
+    "created_time" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "last_modified_time" DATETIME,
+    CONSTRAINT "task_run_task_id_fkey" FOREIGN KEY ("task_id") REFERENCES "task" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- CreateTable
 CREATE TABLE "task_reference" (
-    "id" TEXT NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "from_field_id" TEXT NOT NULL,
     "to_field_id" TEXT NOT NULL,
-    "created_time" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "task_reference_pkey" PRIMARY KEY ("id")
+    "created_time" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+-- CreateIndex
+CREATE INDEX "task_type_status_idx" ON "task"("type", "status");
 
 -- CreateIndex
 CREATE INDEX "task_run_task_id_status_idx" ON "task_run"("task_id", "status");
@@ -47,6 +46,3 @@ CREATE INDEX "task_reference_to_field_id_idx" ON "task_reference"("to_field_id")
 
 -- CreateIndex
 CREATE UNIQUE INDEX "task_reference_to_field_id_from_field_id_key" ON "task_reference"("to_field_id", "from_field_id");
-
--- AddForeignKey
-ALTER TABLE "task_run" ADD CONSTRAINT "task_run_task_id_fkey" FOREIGN KEY ("task_id") REFERENCES "task"("id") ON DELETE CASCADE ON UPDATE CASCADE;
