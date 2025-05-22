39ade029347b65b2145b1442ea34608d488faad0
Boris <boris2code@outlook.com>
2025-05-22 18:24:00 +0800

    fix: delete duplicate records in user_last_visit table

--- packages/db-main-prisma/prisma/postgres/migrations/20250520081803_update_user_last_visit/migration.sql
+++ packages/db-main-prisma/prisma/postgres/migrations/20250520081803_update_user_last_visit/migration.sql
@@ -1,6 +1,20 @@
 BEGIN;
+
 -- DropIndex
 DROP INDEX "user_last_visit_user_id_resource_type_parent_resource_id_key";
+
+-- fix error: Delete duplicate records before creating unique index
+WITH duplicates AS (
+  SELECT user_id, resource_type, resource_id, COUNT(*) as count
+   FROM user_last_visit
+   GROUP BY user_id, resource_type, resource_id
+)
+DELETE FROM user_last_visit
+WHERE (user_id, resource_type, resource_id) IN (
+  SELECT user_id, resource_type, resource_id
+  FROM duplicates
+  WHERE count > 1
+);
 
 -- CreateIndex
 CREATE INDEX "user_last_visit_user_id_resource_type_parent_resource_id_idx" ON "user_last_visit"("user_id", "resource_type", "parent_resource_id");
