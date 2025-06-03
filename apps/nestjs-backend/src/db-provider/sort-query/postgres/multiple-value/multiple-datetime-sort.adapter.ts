265acd6bcc0b6cd5f471605dbd43ba0de2c57c76
caoxing <caoxing9@gmail.com>
2025-06-03 14:47:54 +0800

    perf: multiple datetime sort

--- apps/nestjs-backend/src/db-provider/sort-query/postgres/multiple-value/multiple-datetime-sort.adapter.ts
+++ apps/nestjs-backend/src/db-provider/sort-query/postgres/multiple-value/multiple-datetime-sort.adapter.ts
@@ -1,85 +1,67 @@
-import type { IDateFieldOptions } from '@teable/core';
 import type { Knex } from 'knex';
-import { getPostgresDateTimeFormatString } from '../../format-string';
 import { SortFunctionPostgres } from '../sort-query.function';
 
 export class MultipleDateTimeSortAdapter extends SortFunctionPostgres {
   asc(builderClient: Knex.QueryBuilder): Knex.QueryBuilder {
-    const { options } = this.field;
-    const { timeZone } = (options as IDateFieldOptions).formatting;
-    const formatString = getPostgresDateTimeFormatString();
-
     const orderByColumn = this.knex.raw(
       `
-      (SELECT to_jsonb(array_agg(TO_CHAR(TIMEZONE(?, CAST(elem AS timestamp with time zone)), ?)))
+      (SELECT to_jsonb(array_agg(elem))
       FROM jsonb_array_elements_text(??::jsonb) as elem) ->> 0
       ASC NULLS FIRST,
-      (SELECT to_jsonb(array_agg(TO_CHAR(TIMEZONE(?, CAST(elem AS timestamp with time zone)), ?)))
+      (SELECT to_jsonb(array_agg(elem))
       FROM jsonb_array_elements_text(??::jsonb) as elem)
       ASC NULLS FIRST
       `,
-      [timeZone, formatString, this.columnName, timeZone, formatString, this.columnName]
+      [this.columnName, this.columnName]
     );
     builderClient.orderByRaw(orderByColumn);
     return builderClient;
   }
 
   desc(builderClient: Knex.QueryBuilder): Knex.QueryBuilder {
-    const { options } = this.field;
-    const { timeZone } = (options as IDateFieldOptions).formatting;
-    const formatString = getPostgresDateTimeFormatString();
-
     const orderByColumn = this.knex.raw(
       `
-      (SELECT to_jsonb(array_agg(TO_CHAR(TIMEZONE(?, CAST(elem AS timestamp with time zone)), ?)))
+      (SELECT to_jsonb(array_agg(elem))
       FROM jsonb_array_elements_text(??::jsonb) as elem) ->> 0
       DESC NULLS LAST,
-      (SELECT to_jsonb(array_agg(TO_CHAR(TIMEZONE(?, CAST(elem AS timestamp with time zone)), ?)))
+      (SELECT to_jsonb(array_agg(elem))
       FROM jsonb_array_elements_text(??::jsonb) as elem)
       DESC NULLS LAST
       `,
-      [timeZone, formatString, this.columnName, timeZone, formatString, this.columnName]
+      [this.columnName, this.columnName]
     );
     builderClient.orderByRaw(orderByColumn);
     return builderClient;
   }
 
   getAscSQL() {
-    const { options } = this.field;
-    const { timeZone } = (options as IDateFieldOptions).formatting;
-    const formatString = getPostgresDateTimeFormatString();
-
     return this.knex
       .raw(
         `
-      (SELECT to_jsonb(array_agg(TO_CHAR(TIMEZONE(?, CAST(elem AS timestamp with time zone)), ?)))
+      (SELECT to_jsonb(array_agg(elem))
       FROM jsonb_array_elements_text(??::jsonb) as elem) ->> 0
       ASC NULLS FIRST,
-      (SELECT to_jsonb(array_agg(TO_CHAR(TIMEZONE(?, CAST(elem AS timestamp with time zone)), ?)))
+      (SELECT to_jsonb(array_agg(elem))
       FROM jsonb_array_elements_text(??::jsonb) as elem)
       ASC NULLS FIRST
       `,
-        [timeZone, formatString, this.columnName, timeZone, formatString, this.columnName]
+        [this.columnName, this.columnName]
       )
       .toQuery();
   }
 
   getDescSQL() {
-    const { options } = this.field;
-    const { timeZone } = (options as IDateFieldOptions).formatting;
-    const formatString = getPostgresDateTimeFormatString();
-
     return this.knex
       .raw(
         `
-      (SELECT to_jsonb(array_agg(TO_CHAR(TIMEZONE(?, CAST(elem AS timestamp with time zone)), ?)))
+      (SELECT to_jsonb(array_agg(elem))
       FROM jsonb_array_elements_text(??::jsonb) as elem) ->> 0
       DESC NULLS LAST,
-      (SELECT to_jsonb(array_agg(TO_CHAR(TIMEZONE(?, CAST(elem AS timestamp with time zone)), ?)))
+      (SELECT to_jsonb(array_agg(elem))
       FROM jsonb_array_elements_text(??::jsonb) as elem)
       DESC NULLS LAST
       `,
-        [timeZone, formatString, this.columnName, timeZone, formatString, this.columnName]
+        [this.columnName, this.columnName]
       )
       .toQuery();
   }
--- apps/nestjs-backend/src/db-provider/sort-query/sqlite/multiple-value/multiple-datetime-sort.adapter.ts
+++ apps/nestjs-backend/src/db-provider/sort-query/sqlite/multiple-value/multiple-datetime-sort.adapter.ts
@@ -1,82 +1,59 @@
-import type { IDateFieldOptions } from '@teable/core';
 import type { Knex } from 'knex';
-import { getOffset } from '../../../search-query/get-offset';
-import { getSqliteDateTimeFormatString } from '../../format-string';
 import { SortFunctionSqlite } from '../sort-query.function';
 
 export class MultipleDateTimeSortAdapter extends SortFunctionSqlite {
   asc(builderClient: Knex.QueryBuilder): Knex.QueryBuilder {
-    const { options } = this.field;
-    const { timeZone } = (options as IDateFieldOptions).formatting;
-    const formatString = getSqliteDateTimeFormatString();
-    const offsetString = `${getOffset(timeZone)} hour`;
-
     const orderByColumn = this.knex.raw(
       `
       (
-        SELECT group_concat(strftime(?, DATETIME(elem.value, ?)), ', ')
+        SELECT group_concat(elem.value, ', ')
         FROM json_each(??) as elem
       ) ASC NULLS FIRST
       `,
-      [formatString, offsetString, this.columnName]
+      [this.columnName]
     );
     builderClient.orderByRaw(orderByColumn);
     return builderClient;
   }
 
   desc(builderClient: Knex.QueryBuilder): Knex.QueryBuilder {
-    const { options } = this.field;
-    const { timeZone } = (options as IDateFieldOptions).formatting;
-    const formatString = getSqliteDateTimeFormatString();
-    const offsetString = `${getOffset(timeZone)} hour`;
-
     const orderByColumn = this.knex.raw(
       `
       (
-        SELECT group_concat(strftime(?, DATETIME(elem.value, ?)), ', ')
+        SELECT group_concat(elem.value, ', ')
         FROM json_each(??) as elem
       ) DESC NULLS LAST
       `,
-      [formatString, offsetString, this.columnName]
+      [this.columnName]
     );
     builderClient.orderByRaw(orderByColumn);
     return builderClient;
   }
 
   getAscSQL() {
-    const { options } = this.field;
-    const { timeZone } = (options as IDateFieldOptions).formatting;
-    const formatString = getSqliteDateTimeFormatString();
-    const offsetString = `${getOffset(timeZone)} hour`;
-
     return this.knex
       .raw(
         `
         (
-          SELECT group_concat(strftime(?, DATETIME(elem.value, ?)), ', ')
+          SELECT group_concat(elem.value, ', ')
           FROM json_each(??) as elem
         ) ASC NULLS FIRST
         `,
-        [formatString, offsetString, this.columnName]
+        [this.columnName]
       )
       .toQuery();
   }
 
   getDescSQL() {
-    const { options } = this.field;
-    const { timeZone } = (options as IDateFieldOptions).formatting;
-    const formatString = getSqliteDateTimeFormatString();
-    const offsetString = `${getOffset(timeZone)} hour`;
-
     return this.knex
       .raw(
         `
         (
-          SELECT group_concat(strftime(?, DATETIME(elem.value, ?)), ', ')
+          SELECT group_concat(elem.value, ', ')
           FROM json_each(??) as elem
         ) DESC NULLS LAST
         `,
-        [formatString, offsetString, this.columnName]
+        [this.columnName]
       )
       .toQuery();
   }
