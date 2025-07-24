385340f1147eb94fb7e0b66eb5ab341e33fdb50a
Boris <boris2code@outlook.com>
2025-07-24 11:55:25 +0800

    refactor: remove retryOnUniqueViolation decorator

--- apps/nestjs-backend/src/features/record/open-api/record-open-api.service.ts
+++ apps/nestjs-backend/src/features/record/open-api/record-open-api.service.ts
@@ -20,7 +20,7 @@ import { IThresholdConfig, ThresholdConfig } from '../../../configs/threshold.co
 import { EventEmitterService } from '../../../event-emitter/event-emitter.service';
 import { Events } from '../../../event-emitter/events';
 import type { IClsStore } from '../../../types/cls';
-import { retryOnDeadlock, retryOnUniqueViolation } from '../../../utils/retry-decorator';
+import { retryOnDeadlock } from '../../../utils/retry-decorator';
 import { AttachmentsStorageService } from '../../attachments/attachments-storage.service';
 import { AttachmentsService } from '../../attachments/attachments.service';
 import { getPublicFullStorageUrl } from '../../attachments/plugins/utils';
@@ -54,7 +54,6 @@ export class RecordOpenApiService {
   ) {}
 
   @retryOnDeadlock()
-  @retryOnUniqueViolation()
   async multipleCreateRecords(
     tableId: string,
     createRecordsRo: ICreateRecordsRo,
@@ -264,7 +263,6 @@ export class RecordOpenApiService {
   }
 
   @retryOnDeadlock()
-  @retryOnUniqueViolation()
   async updateRecords(
     tableId: string,
     updateRecordsRo: IUpdateRecordsRo & {
