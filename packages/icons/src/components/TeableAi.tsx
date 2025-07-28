fa094c9dc385960fa5dc17f36107398a4b53c0fc
Aries X <caoxing9@gmail.com>
2025-07-28 14:36:38 +0800

    feat: add more agent for ai chat (#1686)
    * chore: update i18n
    
    * chore: delete useless component
    
    * perf: first frame for ai panel
    
    * perf: add build base agent for complex apps builder task
    
    * chore: add more buildbase operator
    
    * chore: update teable ai icon

--- packages/icons/src/components/TeableAi.tsx
+++ packages/icons/src/components/TeableAi.tsx
@@ -3,62 +3,75 @@ import type { SVGProps } from 'react';
 
 const TeableAi = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    width="58"
-    height="58"
-    viewBox="0 0 58 58"
+    width="698"
+    height="698"
+    viewBox="0 0 698 698"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     {...props}
   >
-    <g opacity="0.9">
-      <g id="Repeat group 1_inner" data-figma-trr="r8u3.7-0f">
-        <rect
-          x="22.8311"
-          y="0.630859"
-          width="12"
-          height="12"
-          rx="2"
-          fill="url(#paint0_linear_7420_3889)"
+    <g clipPath="url(#clip0_7775_23694)">
+      <g filter="url(#filter0_d_7775_23694)">
+        <path
+          d="M349 666.691C526.49 666.691 670.375 522.807 670.375 345.317C670.375 167.826 526.49 23.942 349 23.942C171.51 23.942 27.6252 167.826 27.6252 345.317C27.6252 522.807 171.51 666.691 349 666.691Z"
+          fill="white"
         />
       </g>
-      <use
-        xlinkHref="#Repeat%20group%201_inner"
-        transform="translate(28.8312 -11.9423) rotate(45)"
+      <path
+        d="M257.604 192.456L211.458 394.92C199.463 441.156 186.602 535.725 289.951 480.374C370.201 437.395 383.342 466.413 382.07 480.374"
+        stroke="black"
+        strokeWidth="30"
       />
-      <use xlinkHref="#Repeat%20group%201_inner" transform="translate(57.6625) rotate(90)" />
-      <use
-        xlinkHref="#Repeat%20group%201_inner"
-        transform="translate(69.6048 28.8312) rotate(135)"
+      <path
+        d="M153.781 239.932C176.367 227.489 213.532 228.618 258.061 269.468C312.936 319.808 346.175 320.995 382.15 317.368"
+        stroke="black"
+        strokeWidth="24"
+        strokeLinecap="round"
       />
-      <use
-        xlinkHref="#Repeat%20group%201_inner"
-        transform="translate(57.6625 57.6625) rotate(-180)"
+      <path
+        d="M183.322 136.09L173.801 133.55C161.025 130.142 147.905 137.735 144.496 150.511L123.422 229.504C120.014 242.28 127.608 255.4 140.384 258.808L149.905 261.349C162.681 264.757 175.801 257.163 179.21 244.387L200.284 165.395C203.692 152.619 196.098 139.499 183.322 136.09Z"
+        fill="#077FE8"
       />
-      <use
-        xlinkHref="#Repeat%20group%201_inner"
-        transform="translate(28.8312 69.6048) rotate(-135)"
-      />
-      <use
-        xlinkHref="#Repeat%20group%201_inner"
-        transform="translate(3.11842e-07 57.6625) rotate(-90)"
-      />
-      <use
-        xlinkHref="#Repeat%20group%201_inner"
-        transform="translate(-11.9423 28.8312) rotate(-45)"
+      <path
+        d="M374.571 150.82L365.05 148.28C352.274 144.871 339.154 152.465 335.745 165.241L314.671 244.234C311.263 257.01 318.857 270.13 331.633 273.538L341.154 276.078C353.93 279.487 367.05 271.893 370.459 259.117L391.533 180.124C394.941 167.348 387.347 154.228 374.571 150.82Z"
+        fill="#077FE8"
       />
     </g>
     <defs>
-      <linearGradient
-        id="paint0_linear_7420_3889"
-        x1="25.8867"
-        y1="3.70703"
-        x2="32.8867"
-        y2="10.707"
-        gradientUnits="userSpaceOnUse"
+      <filter
+        id="filter0_d_7775_23694"
+        x="-2.37476"
+        y="-2.05804"
+        width="702.749"
+        height="702.749"
+        filterUnits="userSpaceOnUse"
+        colorInterpolationFilters="sRGB"
       >
-        <stop stopColor="#5E00FF" />
-        <stop offset="1" stopColor="#9150FF" stopOpacity="0" />
-      </linearGradient>
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dy="4" />
+        <feGaussianBlur stdDeviation="15" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0.509804 0 0 0 0 0.894118 0 0 0 0.2 0"
+        />
+        <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_7775_23694" />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_7775_23694"
+          result="shape"
+        />
+      </filter>
+      <clipPath id="clip0_7775_23694">
+        <rect width="698" height="698" fill="white" />
+      </clipPath>
     </defs>
   </svg>
 );
