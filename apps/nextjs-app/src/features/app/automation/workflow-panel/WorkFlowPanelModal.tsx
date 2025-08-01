f524f863e098f1f469f4c51d7e891e84aa602da3
Uno <tianyangchen1@outlook.com>
2025-08-01 14:28:49 +0800

    feat: refactor workflow panel components to support new alert dialog

--- apps/nextjs-app/src/features/app/automation/workflow-panel/WorkFlowPanelModal.tsx
+++ apps/nextjs-app/src/features/app/automation/workflow-panel/WorkFlowPanelModal.tsx
@@ -1,6 +1,19 @@
 import { useIsHydrated } from '@teable/sdk/hooks';
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@teable/ui-lib/shadcn';
-import { lazy, Suspense } from 'react';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  Button,
+  Dialog,
+  DialogContent,
+} from '@teable/ui-lib';
+import { XIcon } from 'lucide-react';
+import { forwardRef, lazy, Suspense, useImperativeHandle, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { tableConfig } from '@/features/i18n/table.config';
 import { useWorkFlowPanelStore } from './useWorkFlowPaneStore';
@@ -10,25 +23,117 @@ const WorkFlowPanelLazy = lazy(() =>
     default: module.WorkFlowPanel,
   }))
 );
+
+interface AlertCloseDialogProps {
+  handleCancel: () => void;
+  handleConfirm: () => void;
+}
+interface AlertCloseDialogRef {
+  open: () => void;
+}
+
+const AlertCloseWorkflowDialog = forwardRef<AlertCloseDialogRef, AlertCloseDialogProps>(
+  (props, ref) => {
+    const { handleCancel, handleConfirm } = props;
+    const [open, setOpen] = useState(false);
+    const { t } = useTranslation(tableConfig.i18nNamespaces);
+
+    useImperativeHandle(
+      ref,
+      () => {
+        return {
+          open: () => setOpen(true),
+        };
+      },
+      []
+    );
+    return (
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogTrigger asChild></AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>{t('common:automation.turnOnTip')}</AlertDialogTitle>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel className="px-5 py-0.5 text-[13px]" onClick={handleCancel}>
+              {t('common:actions.exit')}
+            </AlertDialogCancel>
+            <AlertDialogAction className="px-5 py-0.5 text-[13px]" onClick={handleConfirm}>
+              {t('common:actions.turnOn')}
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    );
+  }
+);
+
+AlertCloseWorkflowDialog.displayName = 'AlertCloseWorkflowDialog';
+
+interface WorkFlowPanelRef {
+  getWorkflow?: () => unknown | undefined;
+  checkCanActive?: () => {
+    canActive: boolean;
+    message: string;
+  };
+  activeWorkflow?: () => Promise<void>;
+}
+
 export const WorkFlowPanelModal = () => {
   const { baseId = '', workflowId = '', closeModal, open } = useWorkFlowPanelStore();
   const isHydrated = useIsHydrated();
   const { t } = useTranslation(tableConfig.i18nNamespaces);
-
+  const workflowRef = useRef<WorkFlowPanelRef>(null);
+  const alertCloseWorkflowDialogRef = useRef<AlertCloseDialogRef>(null);
   if (!isHydrated || !baseId || !workflowId || !open) {
     return null;
   }
+
+  const handleClose = () => {
+    const workflow = workflowRef.current?.getWorkflow?.();
+    const isActive = workflow && (workflow as { isActive: boolean }).isActive;
+    if (!isActive) {
+      const checkRes = workflowRef.current?.checkCanActive?.();
+      if (checkRes?.canActive) {
+        alertCloseWorkflowDialogRef.current?.open();
+        return;
+      }
+    }
+    closeModal();
+  };
+
   return (
-    <Dialog open={open} onOpenChange={(isOpen) => !isOpen && closeModal()}>
-      <DialogContent className="w-500 flex h-screen max-h-none max-w-none flex-col p-0">
-        <DialogHeader className="p-6 pb-0">
-          <DialogTitle>{t('table:field.default.button.automation')}</DialogTitle>
-        </DialogHeader>
-        <div className="flex-1 p-6 pt-4">
-          <Suspense fallback={<div>Loading workflow panel...</div>}>
-            <WorkFlowPanelLazy baseId={baseId} workflowId={workflowId} />
+    <Dialog open={open}>
+      <DialogContent
+        closeable={false}
+        className="flex max-w-7xl p-2"
+        style={{ width: 'calc(100% - 40px)', height: 'calc(100% - 100px)' }}
+      >
+        <div className="flex-1">
+          <Suspense fallback={<div>{t('common.loading')}</div>}>
+            <WorkFlowPanelLazy
+              baseId={baseId}
+              workflowId={workflowId}
+              headLeft={
+                <Button variant={'ghost'} size={'xs'} onClick={handleClose}>
+                  <XIcon className="size-4" />
+                </Button>
+              }
+              ref={workflowRef}
+            />
           </Suspense>
         </div>
+
+        <AlertCloseWorkflowDialog
+          ref={alertCloseWorkflowDialogRef}
+          handleCancel={() => {
+            closeModal();
+          }}
+          handleConfirm={() => {
+            workflowRef.current?.activeWorkflow?.();
+            closeModal();
+          }}
+        />
       </DialogContent>
     </Dialog>
   );
