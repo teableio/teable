98cb0cf331902d88535e315a9ffb6c2fc7a35591
Uno <tianyangchen1@outlook.com>
2025-08-18 13:52:26 +0800

    feat: add notification toast support

--- packages/sdk/src/context/notification/NotificationProvider.tsx
+++ packages/sdk/src/context/notification/NotificationProvider.tsx
@@ -1,10 +1,13 @@
 import type { INotificationBuffer } from '@teable/core';
 import { getUserNotificationChannel } from '@teable/core';
+import { toast } from '@teable/ui-lib/src/shadcn/ui/sonner';
+import { BellRingIcon } from 'lucide-react';
 import type { FC, ReactNode } from 'react';
 import { useEffect, useState } from 'react';
 import type { Presence } from 'sharedb/lib/client';
 import { useSession } from '../../hooks';
 import { useConnection } from '../../hooks/use-connection';
+import { useTranslation } from '../app/i18n';
 import { NotificationContext } from './NotificationContext';
 
 interface INotificationProviderProps {
@@ -14,7 +17,7 @@ interface INotificationProviderProps {
 export const NotificationProvider: FC<INotificationProviderProps> = ({ children }) => {
   const { user } = useSession();
   const { connection } = useConnection();
-
+  const { t } = useTranslation();
   const [remotePresence, setRemotePresence] = useState<Presence>();
   const [notification, setNotification] = useState<INotificationBuffer | null>(null);
 
@@ -29,6 +32,12 @@ export const NotificationProvider: FC<INotificationProviderProps> = ({ children 
     remotePresence?.subscribe((err) => err && console.error);
 
     const receiveHandler = (_id: string, res: INotificationBuffer) => {
+      if (!res.notification.isRead) {
+        toast(t('notification.title'), {
+          description: res.notification.message,
+          icon: <BellRingIcon className="size-4" />,
+        });
+      }
       setNotification(res);
     };
 
@@ -39,7 +48,7 @@ export const NotificationProvider: FC<INotificationProviderProps> = ({ children 
       remotePresence?.unsubscribe();
       remotePresence?.destroy();
     };
-  }, [connection, remotePresence, user.id]);
+  }, [connection, remotePresence, t, user.id]);
 
   return (
     <NotificationContext.Provider value={notification}>{children}</NotificationContext.Provider>
