b7e2520df339aa09f071066e98783dc0e63d6217
Uno <tianyangchen1@outlook.com>
2025-09-01 18:52:08 +0800

    feat: implement proxy handling in PluginStaticProxyMiddleware for static asset requests

--- apps/nestjs-backend/src/features/next/plugin/plugin-static-proxy.middleware.ts
+++ apps/nestjs-backend/src/features/next/plugin/plugin-static-proxy.middleware.ts
@@ -1,18 +1,24 @@
 // plugin-static-proxy.middleware.ts
-import path from 'path';
 import type { NestMiddleware } from '@nestjs/common';
 import type { Request, Response } from 'express';
-
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import type { RequestHandler } from 'http-proxy-middleware';
 export class PluginStaticProxyMiddleware implements NestMiddleware {
+  private proxy: RequestHandler;
+
+  constructor() {
+    const assetPrefix = process.env.NEXT_BUILD_ENV_ASSET_PREFIX;
+    this.proxy = createProxyMiddleware({
+      target: assetPrefix,
+      changeOrigin: true,
+    });
+  }
+
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   async use(req: Request, res: Response, next: () => void): Promise<any> {
     const assetPrefix = process.env.NEXT_BUILD_ENV_ASSET_PREFIX;
-    console.log('PluginStaticProxyMiddleware assetPrefix', assetPrefix);
-    console.log('PluginStaticProxyMiddleware req.path', req.path);
     if (assetPrefix && req.path.startsWith('/plugin/_next/static/')) {
-      const assetUrl = path.join(assetPrefix, req.path);
-      console.log('PluginStaticProxyMiddleware assetUrl', assetUrl);
-      return res.redirect(302, assetUrl);
+      return this.proxy(req, res, next);
     }
 
     next();
