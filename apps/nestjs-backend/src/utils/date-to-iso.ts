a64d9c0928078b4c5f64fa66b5df286adb5c4d28
Boris <boris2code@outlook.com>
2025-09-16 16:11:05 +0800

    fix: more safe typescript

--- apps/nestjs-backend/src/utils/date-to-iso.ts
+++ apps/nestjs-backend/src/utils/date-to-iso.ts
@@ -5,6 +5,12 @@ export const dateToIso = <T extends object>(obj: T) => {
       value instanceof Date ? value.toISOString() : value,
     ])
   ) as {
-    [K in keyof T]: T[K] extends Date ? string : T[K];
+    [K in keyof T]: T[K] extends Date
+      ? string
+      : T[K] extends Date | null
+        ? string | null
+        : T[K] extends Date | undefined
+          ? string | undefined
+          : T[K];
   };
 };
