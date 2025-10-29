12569d86d35fb5662c56d42ffab564fca67dbe5b
Uno <tianyangchen1@outlook.com>
2025-10-29 17:04:30 +0800

    refactor: improve localization message handling in LinkNotification component

--- apps/nextjs-app/src/features/app/components/notifications/notification-component/LinkNotification.tsx
+++ apps/nextjs-app/src/features/app/components/notifications/notification-component/LinkNotification.tsx
@@ -1,4 +1,5 @@
-import { NotificationTypeEnum, type NotificationStatesEnum } from '@teable/core';
+import { NotificationTypeEnum } from '@teable/core';
+import type { ILocalization, NotificationStatesEnum } from '@teable/core';
 import { type INotificationVo } from '@teable/openapi';
 import { getLocalizationMessage } from '@teable/sdk/context';
 import type { ILocaleFunction } from '@teable/sdk/context/app/i18n';
@@ -13,7 +14,13 @@ interface LinkNotificationProps {
 const getShowMessage = (message: string, t: ILocaleFunction) => {
   try {
     const parsedMessage = JSON.parse(message);
-    return getLocalizationMessage(parsedMessage, t);
+    const { i18nKey = '', context } = parsedMessage as ILocalization;
+    if (!i18nKey) {
+      return message;
+    }
+    // replace first . to :
+    // eg: common.email.templates -> common:email:templates
+    return getLocalizationMessage({ i18nKey: i18nKey.replace(/\./, ':'), context }, t);
   } catch (error) {
     return message;
   }
@@ -24,7 +31,7 @@ export const LinkNotification = (props: LinkNotificationProps) => {
     data: { url, message: messageString, notifyType },
   } = props;
 
-  const { t } = useTranslation('system');
+  const { t } = useTranslation(['common']);
   const message = getShowMessage(messageString, t as ILocaleFunction);
 
   return notifyType !== NotificationTypeEnum.ExportBase ? (
