48bffd8f969367cb76a8afbea56b623a6ee7dd2c
Uno <tianyangchen1@outlook.com>
2025-11-05 19:17:59 +0800

    style: enhance toast appearance

--- packages/ui-lib/src/shadcn/ui/sonner.tsx
+++ packages/ui-lib/src/shadcn/ui/sonner.tsx
@@ -17,10 +17,17 @@ const Toaster = ({ ...props }: ToasterProps) => {
       toastOptions={{
         classNames: {
           toast:
-            'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
+            'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg items-start',
           description: 'group-[.toast]:text-muted-foreground',
           actionButton: 'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
           cancelButton: 'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground',
+          icon: 'items-start',
+          closeButton:
+            'top-[16px] right-0 left-[unset] border-none !text-foreground !bg-transparent',
+        },
+        style: {
+          background: 'hsl(var(--background))',
+          borderColor: 'hsl(var(--border))',
         },
       }}
       position={props.position ?? 'top-center'}
