4b8e21209b3b110417852edb4d9caa91a868b567
Uno <tianyangchen1@outlook.com>
2025-11-14 16:23:42 +0800

    fix: test

--- apps/nestjs-backend/src/features/record/record-modify/record-duplicate.service.ts
+++ apps/nestjs-backend/src/features/record/record-modify/record-duplicate.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { FieldKeyType, HttpErrorCode } from '@teable/core';
 import { PrismaService } from '@teable/db-main-prisma';
 import type { IRecordInsertOrderRo, IRecord } from '@teable/openapi';
