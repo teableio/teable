158d61c3ae4a545f4aba890ad2725ab681772d5a
Uno <tianyangchen1@outlook.com>
2025-11-27 10:23:16 +0800

    fix: router

--- apps/nextjs-app/src/pages/base/[baseId]/[tableId].tsx
+++ apps/nextjs-app/src/pages/base/[baseId]/[tableId].tsx
@@ -1,5 +1,32 @@
-import withEnv from '@/lib/withEnv';
-import { getServerSideProps as getServerSidePropsBase } from './table/[tableId]/index';
+import { LastVisitResourceType } from '@teable/openapi';
+import type { GetServerSideProps } from 'next';
+import type { NextPageWithLayout } from '@/lib/type';
+import withAuthSSR from '@/lib/withAuthSSR';
 
-export const getServerSideProps = withEnv(getServerSidePropsBase);
-export { default } from './table/[tableId]/index';
+const Node: NextPageWithLayout = () => {
+  return <p>redirecting</p>;
+};
+
+export const getServerSideProps: GetServerSideProps = withAuthSSR(async (context, ssrApi) => {
+  const { baseId, tableId, ...queryParams } = context.query;
+  const queryString = new URLSearchParams(queryParams as Record<string, string>).toString();
+
+  const userLastVisitView = await ssrApi.getUserLastVisit(
+    LastVisitResourceType.View,
+    tableId as string
+  );
+  if (!userLastVisitView) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    redirect: {
+      destination: `/base/${baseId}/table/${tableId}/${userLastVisitView.resourceId}?${queryString}`,
+      permanent: false,
+    },
+  };
+});
+
+export default Node;
