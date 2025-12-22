83bb428a00494c3a7dbcf99af572f89c2876d6dd
hammond <hammond@teable.io>
2025-12-22 11:47:03 +0800

    feat: automation steps new icons T1353

---
+++ packages/icons/src/components/ActionGetRecord.tsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import type { SVGProps } from 'react';
+const ActionGetRecord = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <rect width="24" height="24" fill="#FFEDD5" fillOpacity="0.1" />
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M7.11085 7.11082C9.25874 4.96294 12.7413 4.96294 14.8892 7.11082C16.7954 9.01714 17.0078 11.9735 15.5303 14.1162L18.4243 17.0102C18.8149 17.4008 18.8148 18.0338 18.4243 18.4243C18.0338 18.8148 17.4008 18.8148 17.0103 18.4243L14.1162 15.5303C11.9735 17.0077 9.01717 16.7953 7.11085 14.8891C4.96297 12.7413 4.96297 9.2587 7.11085 7.11082ZM13.4746 8.52488C12.1078 7.15815 9.89172 7.15808 8.52491 8.52488C7.15811 9.89168 7.15818 12.1077 8.52491 13.4746C9.89175 14.8414 12.1078 14.8414 13.4746 13.4746C14.8415 12.1078 14.8415 9.89172 13.4746 8.52488Z"
+      fill="#F97316"
+    />
+  </svg>
+);
+export default ActionGetRecord;
