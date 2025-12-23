9a8887942837b8336be2d9b480eded1da1ff8df1
Uno <tianyangchen1@outlook.com>
2025-12-23 11:47:04 +0800

    feat: enhance base permission update listener to emit collaborator update event

--- apps/nestjs-backend/src/event-emitter/listeners/base-permission-update.listener.ts
+++ apps/nestjs-backend/src/event-emitter/listeners/base-permission-update.listener.ts
@@ -1,14 +1,21 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { OnEvent } from '@nestjs/event-emitter';
 import { getBasePermissionUpdateChannel } from '@teable/core';
+import { PrismaService } from '@teable/db-main-prisma';
 import { ShareDbService } from '../../share-db/share-db.service';
+import { EventEmitterService } from '../event-emitter.service';
 import { Events, BasePermissionUpdateEvent } from '../events';
+import { CollaboratorUpdateEvent } from '../events/space/collaborator.event';
 
 @Injectable()
 export class BasePermissionUpdateListener {
   private readonly logger = new Logger(BasePermissionUpdateListener.name);
 
-  constructor(private readonly shareDbService: ShareDbService) {}
+  constructor(
+    private readonly shareDbService: ShareDbService,
+    private readonly prismaService: PrismaService,
+    private readonly eventEmitterService: EventEmitterService
+  ) {}
 
   @OnEvent(Events.BASE_PERMISSION_UPDATE, { async: true })
   async basePermissionUpdateListener(listenerEvent: BasePermissionUpdateEvent) {
@@ -16,6 +23,22 @@ export class BasePermissionUpdateListener {
       payload: { baseId },
       context: { user },
     } = listenerEvent;
+    const space = await this.prismaService.base.findUnique({
+      where: {
+        id: baseId,
+      },
+      select: {
+        spaceId: true,
+      },
+    });
+
+    if (space?.spaceId) {
+      this.eventEmitterService.emitAsync(
+        Events.COLLABORATOR_UPDATE,
+        new CollaboratorUpdateEvent(space.spaceId)
+      );
+    }
+
     const channel = getBasePermissionUpdateChannel(baseId);
     const presence = this.shareDbService.connect().getPresence(channel);
     const localPresence = presence.create();
